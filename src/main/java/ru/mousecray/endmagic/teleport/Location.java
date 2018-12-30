29a994c09942f290919d5d782521fcd4b7115574
hohserg <hohserg1@yandex.ru>
2018-12-30 15:22:41 +0300

    Added Location
    Extrract logic of work with nbt

---
+++ src/main/java/ru/mousecray/endmagic/teleport/Location.java
@@ -0,0 +1,27 @@
+package ru.mousecray.endmagic.teleport;
+
+import net.minecraft.nbt.NBTTagCompound;
+
+public class Location {
+    int x, y, z, dim;
+
+    public Location(int x, int y, int z, int dim) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.dim = dim;
+    }
+
+    public NBTTagCompound toNbt() {
+        NBTTagCompound r = new NBTTagCompound();
+        r.setInteger("x", x);
+        r.setInteger("y", y);
+        r.setInteger("z", z);
+        r.setInteger("dim", dim);
+        return r;
+    }
+
+    public static Location fromNbt(NBTTagCompound tag) {
+        return new Location(tag.getInteger("x"), tag.getInteger("y"), tag.getInteger("z"), tag.getInteger("dim"));
+    }
+}
