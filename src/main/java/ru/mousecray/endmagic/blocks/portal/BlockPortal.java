b0e2f2cd4ef6f9e16d972df2ce1e0fb97196160c
hohserg <hohserg1@yandex.ru>
2018-12-30 22:38:16 +0300

    Added perepheral methods

--- src/main/java/ru/mousecray/endmagic/blocks/portal/BlockPortal.java
+++ src/main/java/ru/mousecray/endmagic/blocks/portal/BlockPortal.java
@@ -1,28 +1,34 @@
 package ru.mousecray.endmagic.blocks.portal;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockContainer;
+import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.BlockFaceShape;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ITickable;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import ru.mousecray.endmagic.teleport.Location;
 import ru.mousecray.endmagic.teleport.TeleportUtils;
 
+import javax.annotation.Nullable;
 import java.util.List;
+import java.util.Random;
 
 public class BlockPortal extends BlockContainer {
-    protected BlockPortal() {
+    public BlockPortal() {
         super(Material.PORTAL);
-    }
-
-    @Override
-    public void onEntityCollidedWithBlock(World worldIn, BlockPos pos, IBlockState state, Entity entityIn) {
-        super.onEntityCollidedWithBlock(worldIn, pos, state, entityIn);
     }
 
     @Override
@@ -30,25 +36,7 @@ public class BlockPortal extends BlockContainer {
         return new TilePortal();
     }
 
-    private class TilePortal extends TileEntity implements ITickable {
-        Location distination;
-
-        @Override
-        public NBTTagCompound writeToNBT(NBTTagCompound compound) {
-            compound.setTag("distination", distination.toNbt());
-            return super.writeToNBT(compound);
-        }
-
-        @Override
-        public void readFromNBT(NBTTagCompound compound) {
-            if (compound.hasKey("distination", 10))
-                distination = Location.fromNbt(compound.getCompoundTag("distination"));
-            else
-                distination = new Location(pos, world);
-
-            super.readFromNBT(compound);
-        }
-
+    private class TilePortal extends TileWithLocation implements ITickable {
         int tick = 0;
 
         @Override
@@ -62,7 +50,41 @@ public class BlockPortal extends BlockContainer {
 
                 list.forEach(it -> TeleportUtils.teleportToLocation(it, distination));
             }
-
         }
+    }
+
+    @SideOnly(Side.CLIENT)
+    public boolean shouldSideBeRendered(IBlockState blockState, IBlockAccess blockAccess, BlockPos pos, EnumFacing side) {
+        IBlockState iblockstate = blockAccess.getBlockState(pos.offset(side));
+        Block block = iblockstate.getBlock();
+        return !iblockstate.isOpaqueCube() && block != Blocks.END_GATEWAY;
+    }
+
+    public AxisAlignedBB getCollisionBoundingBox(IBlockState blockState, IBlockAccess worldIn, BlockPos pos) {
+        return NULL_AABB;
+    }
+
+    public boolean isOpaqueCube(IBlockState state) {
+        return false;
+    }
+
+    public boolean isFullCube(IBlockState state) {
+        return false;
+    }
+
+    public int quantityDropped(Random random) {
+        return 0;
+    }
+
+    public ItemStack getItem(World worldIn, BlockPos pos, IBlockState state) {
+        return ItemStack.EMPTY;
+    }
+
+    public MapColor getMapColor(IBlockState state, IBlockAccess worldIn, BlockPos pos) {
+        return MapColor.BLACK;
+    }
+
+    public BlockFaceShape getBlockFaceShape(IBlockAccess worldIn, IBlockState state, BlockPos pos, EnumFacing face) {
+        return BlockFaceShape.UNDEFINED;
     }
 }
