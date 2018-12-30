85274cd070ea6d739cc0ed392be361122db2e5b2
hohserg <hohserg1@yandex.ru>
2018-12-31 01:29:05 +0300

    Beautify code

--- src/main/java/ru/mousecray/endmagic/init/EMBlocks.java
+++ src/main/java/ru/mousecray/endmagic/init/EMBlocks.java
@@ -1,7 +1,7 @@
 package ru.mousecray.endmagic.init;
 
-import net.minecraft.block.Block;
-import ru.mousecray.endmagic.blocks.*;
+import ru.mousecray.endmagic.blocks.EnderCrops;
+import ru.mousecray.endmagic.blocks.EnderGrass;
 import ru.mousecray.endmagic.blocks.portal.BlockPortal;
 import ru.mousecray.endmagic.blocks.portal.BlockTopMark;
 import ru.mousecray.endmagic.blocks.portal.MasterBlockPortal;
@@ -11,10 +11,10 @@ public final class EMBlocks {
 
     public static final BlockPortal blockPortal = new BlockPortal();
 
-    public static final MasterBlockPortal materBlockPortal = new MasterBlockPortal();
+    public static final MasterBlockPortal masterBlockPortal = new MasterBlockPortal();
 
     public static final BlockTopMark blockTopMark = new BlockTopMark();
-  
-	  public static final EnderCrops enderCrops = new EnderCrops();
+
+    public static final EnderCrops enderCrops = new EnderCrops();
 
 }
