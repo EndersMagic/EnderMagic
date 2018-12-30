e9b52afd49edf698c26d432b5696be6468b7cd0b
hohserg <hohserg1@yandex.ru>
2018-12-30 18:27:18 +0300

    Added tile entityes registration

--- src/main/java/ru/mousecray/endmagic/proxy/CommonProxy.java
+++ src/main/java/ru/mousecray/endmagic/proxy/CommonProxy.java
@@ -5,15 +5,19 @@ import java.util.LinkedList;
 import java.util.List;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 import ru.mousecray.endmagic.EM;
 import ru.mousecray.endmagic.init.EMBlocks;
 import ru.mousecray.endmagic.util.NameUtils;
@@ -21,6 +25,7 @@ import ru.mousecray.endmagic.util.NameUtils;
 public class CommonProxy {
 
     protected List<Item> itemsToRegister = new LinkedList<>();
+    protected List<Class<? extends TileEntity>> tilesToRegister = new LinkedList<>();
     protected List<Block> blocksToRegister = new LinkedList<>();
 
     public void preInit(FMLPreInitializationEvent event) {
@@ -43,8 +48,16 @@ public class CommonProxy {
         block.setUnlocalizedName(name);
         block.setCreativeTab(EM.EM_CREATIVE);
 
+        if (block instanceof ITileEntityProvider) {
+            Class<? extends TileEntity> tile = ((ITileEntityProvider) block).createNewTileEntity(null, 0).getClass();
+            registerTile(tile);
+        }
         blocksToRegister.add(block);
         registerItem(new ItemBlock(block), block.getRegistryName().toString());
+    }
+
+    private void registerTile(Class<? extends TileEntity> tile) {
+        tilesToRegister.add(tile);
     }
 
     private void registerItem(Item item, String name) {
@@ -62,6 +75,7 @@ public class CommonProxy {
     @SubscribeEvent
     public void registerBlocks(RegistryEvent.Register<Block> e) {
         blocksToRegister.forEach(e.getRegistry()::register);
+        tilesToRegister.forEach(tile -> GameRegistry.registerTileEntity(tile, new ResourceLocation(EM.ID, tile.getSimpleName())));
     }
 
     @SubscribeEvent
