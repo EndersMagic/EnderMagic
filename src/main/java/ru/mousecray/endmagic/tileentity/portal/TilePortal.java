ca6a3c7e0c442225021858146982ae0c6b250b9a
hohserg <hohserg1@yandex.ru>
2018-12-31 01:23:37 +0300

    Beautify code

--- src/main/java/ru/mousecray/endmagic/tileentity/portal/TilePortal.java
+++ src/main/java/ru/mousecray/endmagic/tileentity/portal/TilePortal.java
@@ -1,11 +1,6 @@
 package ru.mousecray.endmagic.tileentity.portal;
 
-import net.minecraft.entity.Entity;
 import net.minecraft.util.ITickable;
-import net.minecraft.util.math.AxisAlignedBB;
-import ru.mousecray.endmagic.teleport.TeleportUtils;
-
-import java.util.List;
 
 public class TilePortal extends TileWithLocation implements ITickable {
     private int tick = 0;
