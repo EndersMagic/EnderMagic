5e828662e393c6da6544714824ca3d5d72b1f820
mousecray <mousecray@github.com>
2019-01-07 17:48:08 +0300

    Create DefaultBookChapter

--- src/main/java/ru/mousecray/endmagic/client/gui/DefaultBookChapter.java
+++ src/main/java/ru/mousecray/endmagic/client/gui/DefaultBookChapter.java
@@ -1,5 +1,6 @@
 package ru.mousecray.endmagic.client.gui;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import net.minecraft.client.resources.I18n;
@@ -7,19 +8,28 @@ import ru.mousecray.endmagic.api.embook.BookChapter;
 import ru.mousecray.endmagic.api.embook.IChapterComponent;
 
 public class DefaultBookChapter extends BookChapter {
+	
+	private List<IChapterComponent> components;
 
 	public DefaultBookChapter(int key) {
 		super(key);
+		components = new ArrayList<IChapterComponent>();
 	}
 
 	@Override
 	public String getChapterTitle() {
-		return I18n.format("embook.chapter_title." + String.valueOf(getKey()));
+		return I18n.format("embook.chapter_title." + getKey());
+	}
+	
+	public DefaultBookChapter build(IChapterComponent... elements) {
+		for(IChapterComponent component : elements) {
+			components.add(component);
+		}
+		return this;
 	}
 
 	@Override
-	public List<IChapterComponent> getChapterComponents() {
-		return null;
+	public List<IChapterComponent> getChapterComponents() {		
+		return components;
 	}
-
 }
