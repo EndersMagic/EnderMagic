ab11906f17bd88c8cea068b45af1b35719e9c01f
hohserg <hohserg1@yandex.ru>
2019-03-30 03:59:19 +0300

    Added coords for lines

--- src/main/java/ru/mousecray/endmagic/api/embook/pages/TextPage.java
+++ src/main/java/ru/mousecray/endmagic/api/embook/pages/TextPage.java
@@ -1,18 +1,23 @@
 package ru.mousecray.endmagic.api.embook.pages;
 
+import org.apache.commons.lang3.tuple.Pair;
 import ru.mousecray.endmagic.api.embook.IPage;
 import ru.mousecray.endmagic.api.embook.IStructuralGuiElement;
 import ru.mousecray.endmagic.client.gui.elements.TextLine;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class TextPage implements IPage {
 
     private List<IStructuralGuiElement> lines;
 
     public TextPage(List<String> lines) {
-        this.lines = lines.stream().map(TextLine::new).collect(Collectors.toList());
+        this.lines = IntStream.range(0, lines.size())
+                .mapToObj(i -> Pair.of(i, lines.get(i)))
+                .map(line -> new TextLine(line.getRight(), 0, line.getLeft() * 20))
+                .collect(Collectors.toList());
     }
 
     @Override
