9b09069aea069be53d0e13a8b3ab1767bec4ea22
hohserg <hohserg1@yandex.ru>
2019-03-31 00:26:30 +0300

    Added image render element

---
+++ src/main/java/ru/mousecray/endmagic/client/gui/elements/ImageView.java
@@ -0,0 +1,22 @@
+package ru.mousecray.endmagic.client.gui.elements;
+
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.util.ResourceLocation;
+import org.lwjgl.util.Rectangle;
+import ru.mousecray.endmagic.api.embook.IStructuralGuiElement;
+
+public class ImageView extends GuiScreen implements IStructuralGuiElement {
+    public final ResourceLocation texture;
+    public final Rectangle rectangle;
+
+    public ImageView(ResourceLocation texture, Rectangle rectangle) {
+        this.texture = texture;
+        this.rectangle = rectangle;
+    }
+
+    @Override
+    public void render(int mouseX, int mouseY) {
+        mc().getTextureManager().bindTexture(texture);
+        drawTexturedModalRect(rectangle.getX(), rectangle.getY(), 0, 0, rectangle.getWidth(), rectangle.getHeight());
+    }
+}
