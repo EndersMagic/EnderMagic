9fc363650bea7f9bdcfead54fc39b4afe2c3e22c
hohserg <hohserg1@yandex.ru>
2019-03-31 03:35:56 +0300

    Added ImageComponent and ImagePage

---
+++ src/main/java/ru/mousecray/endmagic/api/embook/pages/ImagePage.java
@@ -0,0 +1,30 @@
+package ru.mousecray.endmagic.api.embook.pages;
+
+import com.google.common.collect.ImmutableList;
+import net.minecraft.util.ResourceLocation;
+import org.lwjgl.util.Rectangle;
+import ru.mousecray.endmagic.api.embook.BookApi;
+import ru.mousecray.endmagic.api.embook.IPage;
+import ru.mousecray.endmagic.api.embook.IStructuralGuiElement;
+import ru.mousecray.endmagic.client.gui.elements.ImageView;
+import ru.mousecray.endmagic.client.gui.elements.TextLine;
+
+import java.util.List;
+
+public class ImagePage implements IPage {
+    private final ResourceLocation texture;
+    private final String label;
+
+    public ImagePage(ResourceLocation texture, String label) {
+        this.texture = texture;
+        this.label = label;
+    }
+
+    @Override
+    public List<IStructuralGuiElement> elements() {
+        return ImmutableList.of(
+                new ImageView(texture, new Rectangle(20, 20, BookApi.pageWidth - 20, BookApi.pageHeight - 20)),
+                new TextLine(label, BookApi.pageWidth / 2, BookApi.pageHeight - 20)
+        );
+    }
+}
