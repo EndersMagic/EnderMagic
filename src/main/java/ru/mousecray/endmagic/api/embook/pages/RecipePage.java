c43ceac823c0db62f353ec6b2bba9fd1b8f1c463
hohserg <hohserg1@yandex.ru>
2019-03-31 03:19:18 +0300

    Implement RecipePage#elements

--- src/main/java/ru/mousecray/endmagic/api/embook/pages/RecipePage.java
+++ src/main/java/ru/mousecray/endmagic/api/embook/pages/RecipePage.java
@@ -5,9 +5,15 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.util.NonNullList;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import org.lwjgl.util.Rectangle;
+import ru.mousecray.endmagic.EM;
+import ru.mousecray.endmagic.api.embook.BookApi;
 import ru.mousecray.endmagic.api.embook.IPage;
 import ru.mousecray.endmagic.api.embook.IStructuralGuiElement;
+import ru.mousecray.endmagic.client.gui.elements.ImageView;
+import ru.mousecray.endmagic.client.gui.elements.ItemStackView;
 
 import java.util.List;
 import java.util.Optional;
@@ -59,6 +65,15 @@ public class RecipePage implements IPage {
 
     @Override
     public List<IStructuralGuiElement> elements() {
-        return null;
+        return Stream.concat(
+                Stream.of(
+                        new ImageView(new ResourceLocation(EM.ID, "crafting_grid"), new Rectangle(0, 0, BookApi.pageWidth, BookApi.pageHeight)),
+                        new ItemStackView(result, 0, 0)//todo: correct pos
+                ),
+                cratingGrid
+                        .stream()
+                        .flatMap(col -> col.stream()
+                                .map(i -> new ItemStackView(ImmutableList.copyOf(i.matchingStacks), 0, 0 /*todo: correct pos*/))))
+                .collect(ImmutableList.toImmutableList());
     }
 }
