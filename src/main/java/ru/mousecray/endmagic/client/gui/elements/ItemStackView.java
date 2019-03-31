6e114b539f2e303d38b918ed39f23066973ff15c
hohserg <hohserg1@yandex.ru>
2019-03-31 03:18:34 +0300

    Added cyclic items feature

--- src/main/java/ru/mousecray/endmagic/client/gui/elements/ItemStackView.java
+++ src/main/java/ru/mousecray/endmagic/client/gui/elements/ItemStackView.java
@@ -1,5 +1,6 @@
 package ru.mousecray.endmagic.client.gui.elements;
 
+import com.google.common.collect.ImmutableList;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.ItemRenderer;
 import net.minecraft.client.renderer.block.model.ItemCameraTransforms;
@@ -7,11 +8,17 @@ import net.minecraft.item.ItemStack;
 import ru.mousecray.endmagic.api.embook.IStructuralGuiElement;
 
 public class ItemStackView implements IStructuralGuiElement {
-    public final ItemStack itemStack;
+    public final ImmutableList<ItemStack> itemStack;
     public final int x, y;
     private ItemRenderer itemRenderer = mc().getItemRenderer();
 
     public ItemStackView(ItemStack itemStack, int x, int y) {
+        this.itemStack = ImmutableList.of(itemStack);
+        this.x = x;
+        this.y = y;
+    }
+
+    public ItemStackView(ImmutableList<ItemStack> itemStack, int x, int y) {
         this.itemStack = itemStack;
         this.x = x;
         this.y = y;
@@ -20,7 +27,14 @@ public class ItemStackView implements IStructuralGuiElement {
     @Override
     public void render(int mouseX, int mouseY) {
         GlStateManager.translate(x, y, 0);
-        itemRenderer.renderItem(mc().player, itemStack, ItemCameraTransforms.TransformType.GUI);
+        itemRenderer.renderItem(mc().player, cycleItemStack(itemStack), ItemCameraTransforms.TransformType.GUI);
         GlStateManager.translate(-x, -y, 0);
+    }
+
+    private ItemStack cycleItemStack(ImmutableList<ItemStack> itemStack) {
+        if ((itemStack.size() > 0))
+            return itemStack.get((int) (System.currentTimeMillis() / 1000L % itemStack.size()));
+        else
+            return ItemStack.EMPTY;
     }
 }
