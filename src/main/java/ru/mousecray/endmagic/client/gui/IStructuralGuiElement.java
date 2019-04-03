45135d016c849616d52e851d24601d714f5be058
hohserg <hohserg1@yandex.ru>
2019-04-03 17:54:38 +0300

    Added ;

--- src/main/java/ru/mousecray/endmagic/client/gui/IStructuralGuiElement.java
+++ src/main/java/ru/mousecray/endmagic/client/gui/IStructuralGuiElement.java
@@ -3,7 +3,9 @@ package ru.mousecray.endmagic.client.gui;
 import net.minecraft.client.Minecraft;
 
 public interface IStructuralGuiElement {
-    default Minecraft mc(){return Minecraft.getMinecraft()}
+    default Minecraft mc() {
+        return Minecraft.getMinecraft();
+    }
 
     void render(int mouseX, int mouseY);
 }
