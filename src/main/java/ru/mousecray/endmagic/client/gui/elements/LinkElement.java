7b05620e4eea81b80bf5a80706eedb634c9f1540
hohserg <hohserg1@yandex.ru>
2019-04-07 00:21:15 +0300

    Added simple link: allow to open some page on click

---
+++ src/main/java/ru/mousecray/endmagic/client/gui/elements/LinkElement.java
@@ -0,0 +1,30 @@
+package ru.mousecray.endmagic.client.gui.elements;
+
+import ru.mousecray.endmagic.api.embook.PageContainer;
+import ru.mousecray.endmagic.api.embook.alignment.Rectangle;
+import ru.mousecray.endmagic.client.gui.GuiScreenEMBook;
+import ru.mousecray.endmagic.client.gui.IClickable;
+
+public class LinkElement extends TextLine implements IClickable {
+    public final PageContainer page;
+
+    public LinkElement(String name, PageContainer page, int x, int y) {
+        super(name, x, y);
+        this.page = page;
+    }
+
+    @Override
+    public void render(int mouseX, int mouseY) {
+        super.render(mouseX, mouseY);
+    }
+
+    @Override
+    public Rectangle area() {
+        return new Rectangle(x, y, x + mc().fontRenderer.getStringWidth(line), y + mc().fontRenderer.FONT_HEIGHT);
+    }
+
+    @Override
+    public void click() {
+        GuiScreenEMBook.instance.setCurrentPage(page);
+    }
+}
