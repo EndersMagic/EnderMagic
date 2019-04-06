6bd2e263f7837ad98748887dfd86400cafd8b52e
hohserg <hohserg1@yandex.ru>
2019-04-07 00:21:31 +0300

    Update for two page container

--- src/main/java/ru/mousecray/endmagic/client/gui/GuiScreenEMBook.java
+++ src/main/java/ru/mousecray/endmagic/client/gui/GuiScreenEMBook.java
@@ -1,56 +1,59 @@
 package ru.mousecray.endmagic.client.gui;
 
-import java.util.List;
-
+import com.google.common.collect.ImmutableMap;
+import com.sun.istack.internal.NotNull;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.RenderHelper;
-import net.minecraft.client.renderer.RenderItem;
-import net.minecraft.client.resources.I18n;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import ru.mousecray.endmagic.EM;
 import ru.mousecray.endmagic.api.embook.BookApi;
-import ru.mousecray.endmagic.api.embook.components.BookChapter;
+import ru.mousecray.endmagic.api.embook.IPage;
+import ru.mousecray.endmagic.api.embook.PageContainer;
+
+import java.io.IOException;
+import java.util.Optional;
 
 @SideOnly(Side.CLIENT)
 public class GuiScreenEMBook extends GuiScreen {
-	
-	public static GuiScreenEMBook instance = new GuiScreenEMBook();
-	
-	private int updateCount;
-	private GuiButton buttonDone;
-	private NextPageButton buttonNextPage, buttonPreviousPage;
-	
-	//These two variables are related
-	private int currChapter = 0;
-//	private int countPages = 0;
-	
-	
-	
-	private int currPage = 0;
-	
-	private List<BookChapter> bookContent = BookApi.getBookContent();
-//	private Map<String, ChapterPage> pages = new HashMap();
-	
+
+    public static GuiScreenEMBook instance = new GuiScreenEMBook();
+
+    private GuiButton buttonBack;
+    private NextPageButton buttonNextPage, buttonPreviousPage;
+
+    public void setCurrentPage(Optional<PageContainer> currentPage) {
+        this.currentPage = currentPage.orElse(BookApi.mainPage());
+        updateButtons();
+    }
+
+    public void setCurrentPage(@NotNull PageContainer page) {
+        currentPage = page;
+        updateButtons();
+    }
+
+    private PageContainer currentPage = BookApi.mainPage();
+
+    private ImmutableMap<String, PageContainer> bookContent = BookApi.getBookContent();
+
     private static final ResourceLocation BOOK_TEXTURES = new ResourceLocation(EM.ID, "textures/gui/book.png");
-    
+
     @Override
     public void initGui() {
         buttonList.clear();
-        
-        buttonDone = addButton(new GuiButton(0, width / 2 - 100, height / 2 + 98, 200, 20, I18n.format("gui.done")));
-        
+
+        buttonBack = addButton(new BackButton(0, width / 2 - 100, height / 2 + 98));
+
         int i = (width - 256) / 2;
         int j = (height - 192) / 2;
-        buttonNextPage = (NextPageButton)addButton(new NextPageButton(1, i + 215, j + 160, true));
-        buttonPreviousPage = (NextPageButton)addButton(new NextPageButton(2, i + 18, j + 160, false));
-        
+
+        buttonNextPage = addButton(new NextPageButton(1, i + 215, j + 160, true));
+        buttonPreviousPage = addButton(new NextPageButton(2, i + 18, j + 160, false));
+
 //        for(BookChapter chapter : bookContent.values()) {
 //        	for(IPageComponent component : chapter.getChapterComponents()) {
 //        		if(component.getComponentType() == ComponentType.LINK) {
@@ -59,75 +62,63 @@ public class GuiScreenEMBook extends GuiScreen {
 //        	}       	
 //        	genPage(chapter, 0);
 //        }
-        
+
         updateButtons();
     }
-    
-//    private void genPage(BookChapter chapter, int startIndex) {
-//		int guiHeight = (width - 192);
-//		List<IPageComponent> components = chapter.getChapterComponents();
-//		int size = components.size();
-//		List<IPageComponent> currComponents = new ArrayList();
-//		int currSize = 0;
-//		for(int i = startIndex; i < size; i++) {
-//			IPageComponent component = components.get(i);
-//			if(component.getComponentType() == ComponentType.LINK) {
-//				int testSize = currSize + ((PageButton)component).height;
-//				if(testSize  < guiHeight) {
-//					currSize = testSize;
-//					currComponents.add(component);
-//				}
-//				else {
-//					pages.put(chapter.getKey(), new ChapterPage(currComponents));
-//					genPage(chapter, i);
-//					break;
-//				}
-//			}
-//			else {
-//				int testSize = currSize + ((PageComponent)component).getHeight();
-//				if(testSize  < guiHeight) {
-//					currSize = testSize;
-//					currComponents.add(component);
-//				}
-//				else {
-//					pages.put(chapter.getKey(), new ChapterPage(currComponents));
-//					genPage(chapter, i);
-//					break;
-//				}
-//			}
-//		}
-//	}
 
-	@Override
+    @Override
+    protected void mouseClicked(int mouseX, int mouseY, int mouseButton) throws IOException {
+        super.mouseClicked(mouseX, mouseY, mouseButton);
+        int i = (width - BookApi.pageWidth * 2) / 2;
+        int j = (height - BookApi.pageHeight) / 2;
+        if (mouseX < i + BookApi.pageWidth)
+            performClickOnPage(mouseX, mouseY, i, j, currentPage.page1);
+        else
+            performClickOnPage(mouseX, mouseY, i + BookApi.pageWidth, i, currentPage.page2);
+    }
+
+    private void performClickOnPage(int mouseX, int mouseY, int i, int j, IPage page) {
+        page.elements().forEach(e -> {
+            if (e instanceof IClickable && ((IClickable) e).area().contains(mouseX - i, mouseY - j, width, height))
+                ((IClickable) e).click();
+
+        });
+    }
+
+    @Override
+    protected void actionPerformed(GuiButton button) {
+        if (button == buttonNextPage)
+            setCurrentPage(currentPage.right);
+        else if (button == buttonPreviousPage)
+            setCurrentPage(currentPage.left);
+        else if (button == buttonBack)
+            setCurrentPage(BookApi.mainPage());
+    }
+
+    @Override
     public void updateScreen() {
         super.updateScreen();
-        ++updateCount;
     }
-    
+
     private void updateButtons() {
-    	
-    	//TODO: visible nextButtonPage
-    	
-    	buttonPreviousPage.visible = !(currChapter == 0);
-    	buttonDone.visible = true;
-    	
-//    	for(int i = 3; i < buttonList.size(); i++) {
-//    		GuiButton button = buttonList.get(i);
-//    		if(currChapter == ((PageButton)button).getChapterVisible()) {
-//    			button.visible = true;
-//    		}
-//    	}
+        buttonPreviousPage.visible = currentPage.left.isPresent();
+        buttonNextPage.visible = currentPage.right.isPresent();
+        buttonBack.visible = currentPage != BookApi.mainPage();
     }
-	
+
     @Override
     public void drawScreen(int mouseX, int mouseY, float partialTicks) {
-    	
+
         GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
         mc.getTextureManager().bindTexture(BOOK_TEXTURES);
         int i = (width - 256) / 2;
         int j = (height - 192) / 2;
-        drawTexturedModalRect(i, j, 0, 0, 256, 192);
-        
+        drawTexturedModalRect(i, j, 0, 0, BookApi.pageWidth * 2, BookApi.pageHeight);
+
+        drawPage(i, j, currentPage.page1, mouseX, mouseY);
+        drawPage(i + BookApi.pageWidth, j, currentPage.page2, mouseX, mouseY);
+
+
         GlStateManager.disableRescaleNormal();
         RenderHelper.disableStandardItemLighting();
         GlStateManager.disableLighting();
@@ -135,54 +126,43 @@ public class GuiScreenEMBook extends GuiScreen {
         super.drawScreen(mouseX, mouseY, partialTicks);
         RenderHelper.enableGUIStandardItemLighting();
         GlStateManager.enableRescaleNormal();
-        
-    	//TODO: Parsing chapters to page
     }
-    
-    public void drawItemStack(ItemStack stack, int x, int y, String altText) {
-		RenderItem itemRender = mc.getRenderItem();
-		FontRenderer fontRenderer = mc.fontRenderer;
-        GlStateManager.translate(0.0F, 0.0F, 32.0F);
-        this.zLevel = 200.0F;
-        itemRender.zLevel = 200.0F;
-        FontRenderer font = stack.getItem().getFontRenderer(stack);
-        if (font == null) font = fontRenderer;
-        itemRender.renderItemAndEffectIntoGUI(stack, x, y);
-        itemRender.renderItemOverlayIntoGUI(font, stack, x, y, altText);
-        itemRender.zLevel = 0.0F;
-        this.zLevel = 0.0F;
+
+    private void drawPage(int i, int j, IPage page, int mouseX, int mouseY) {
+        GlStateManager.pushMatrix();
+
+        GlStateManager.translate(i, j, 0);
+        page.elements().forEach(e -> e.render(mouseX - i, mouseY - j));
+
+        GlStateManager.popMatrix();
     }
-    
-    @Override
-    public void drawGradientRect(int left, int top, int right, int bottom, int startColor, int endColor) {
-    	super.drawGradientRect(left, top, right, bottom, startColor, endColor);
-    }
-    
-    @Override
-    public void renderToolTip(ItemStack stack, int x, int y) {
-    	super.renderToolTip(stack, x, y);
-    }
-    
+
     @SideOnly(Side.CLIENT)
     static class NextPageButton extends GuiButton {
-    	private final boolean isForward;
+        private final boolean isForward;
 
-    	public NextPageButton(int button, int x, int y, boolean isForward) {
-    		super(button, x, y, 23, 13, "");
-    		this.isForward = isForward;
+        public NextPageButton(int button, int x, int y, boolean isForward) {
+            super(button, x, y, 23, 13, "");
+            this.isForward = isForward;
         }
-    	
-    	@Override
-    	public void drawButton(Minecraft mc, int mouseX, int mouseY, float partialTicks) {
-    		if (visible) {
-    			boolean flag = mouseX >= x && mouseY >= y && mouseX < x + width && mouseY < y + height;
-    			GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-    			mc.getTextureManager().bindTexture(BOOK_TEXTURES);
-    			int i = 0, j = 192;
-    			if (flag) i += 23;
-    			if (!isForward) j += 13;
-    			drawTexturedModalRect(x, y, i, j, 23, 13);
-    		}
-    	}
+
+        @Override
+        public void drawButton(Minecraft mc, int mouseX, int mouseY, float partialTicks) {
+            if (visible) {
+                boolean flag = mouseX >= x && mouseY >= y && mouseX < x + width && mouseY < y + height;
+                GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+                mc.getTextureManager().bindTexture(BOOK_TEXTURES);
+                int i = 0, j = 192;
+                if (flag) i += 23;
+                if (!isForward) j += 13;
+                drawTexturedModalRect(x, y, i, j, 23, 13);
+            }
+        }
+    }
+
+    private class BackButton extends GuiButton {
+        public BackButton(int button, int x, int y) {
+            super(button, x, y, 23, 13, "");
+        }
     }
 }
