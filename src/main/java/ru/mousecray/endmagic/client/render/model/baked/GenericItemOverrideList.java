524614d540780e9ee473b112d368b34b6a5a23ce
hohserg <hohserg1@yandex.ru>
2019-04-30 07:20:57 +0300

    Use custom functional interface

--- src/main/java/ru/mousecray/endmagic/client/render/model/baked/GenericItemOverrideList.java
+++ src/main/java/ru/mousecray/endmagic/client/render/model/baked/GenericItemOverrideList.java
@@ -5,18 +5,21 @@ import net.minecraft.client.renderer.block.model.ItemOverrideList;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
-import scala.Function4;
 
 import javax.annotation.Nullable;
 import java.util.Collections;
 
 public class GenericItemOverrideList {
-    public static ItemOverrideList fromLambda(Function4<IBakedModel, ItemStack, World, EntityLivingBase, IBakedModel> handleItemState) {
+    public static ItemOverrideList fromLambda(HandleItemState handleItemState) {
         return new ItemOverrideList(Collections.emptyList()) {
             @Override
             public IBakedModel handleItemState(IBakedModel originalModel, ItemStack stack, @Nullable World world, @Nullable EntityLivingBase entity) {
                 return handleItemState.apply(originalModel, stack, world, entity);
             }
         };
+    }
+
+    public static interface HandleItemState {
+        public IBakedModel apply(IBakedModel originalModel, ItemStack stack, World world, EntityLivingBase entity);
     }
 }
