342add2cedbeb8a75a91dc29359a84f8bccfe8d8
hohserg <hohserg1@yandex.ru>
2019-04-30 16:50:33 +0300

    Correct tabs

--- src/main/java/ru/mousecray/endmagic/util/elix_x/baked/pipeline/vertex/VertexMatrixTransformer.java
+++ src/main/java/ru/mousecray/endmagic/util/elix_x/baked/pipeline/vertex/VertexMatrixTransformer.java
@@ -1,46 +1,47 @@
 package ru.mousecray.endmagic.util.elix_x.baked.pipeline.vertex;
 
-import ru.mousecray.endmagic.util.elix_x.ecomms.pipeline.PipelineElement;
-import ru.mousecray.endmagic.util.elix_x.baked.vertex.DefaultUnpackedVertex;
 import net.minecraft.util.EnumFacing;
 import net.minecraftforge.common.model.TRSRTransformation;
 import org.lwjgl.util.vector.Matrix4f;
+import org.lwjgl.util.vector.Vector3f;
+import ru.mousecray.endmagic.util.elix_x.baked.vertex.DefaultUnpackedVertex;
+import ru.mousecray.endmagic.util.elix_x.ecomms.pipeline.PipelineElement;
 
 
 public class VertexMatrixTransformer implements PipelineElement<DefaultUnpackedVertex, DefaultUnpackedVertex> {
 
-	public static VertexMatrixTransformer toFace(EnumFacing facing){
-		return new VertexMatrixTransformer(TRSRTransformation.getMatrix(facing));
-	}
+    public static VertexMatrixTransformer toFace(EnumFacing facing) {
+        return new VertexMatrixTransformer(TRSRTransformation.getMatrix(facing));
+    }
 
-	private javax.vecmath.Matrix4f matrix;
+    private javax.vecmath.Matrix4f matrix;
 
-	public VertexMatrixTransformer(Matrix4f matrix){
-		setMatrix(matrix);
-	}
+    public VertexMatrixTransformer(Matrix4f matrix) {
+        setMatrix(matrix);
+    }
 
-	public VertexMatrixTransformer(javax.vecmath.Matrix4f matrix){
-		setMatrix(matrix);
-	}
+    public VertexMatrixTransformer(javax.vecmath.Matrix4f matrix) {
+        setMatrix(matrix);
+    }
 
-	public Matrix4f getMatrix(){
-		return TRSRTransformation.toLwjgl(matrix);
-	}
+    public Matrix4f getMatrix() {
+        return TRSRTransformation.toLwjgl(matrix);
+    }
 
-	public void setMatrix(Matrix4f matrix){
-		this.matrix = TRSRTransformation.toVecmath(matrix);
-	}
+    public void setMatrix(Matrix4f matrix) {
+        this.matrix = TRSRTransformation.toVecmath(matrix);
+    }
 
-	public void setMatrix(javax.vecmath.Matrix4f matrix){
-		this.matrix = matrix;
-	}
+    public void setMatrix(javax.vecmath.Matrix4f matrix) {
+        this.matrix = matrix;
+    }
 
-	@Override
-	public DefaultUnpackedVertex pipe(DefaultUnpackedVertex in){
-		matrix.transform(TRSRTransformation.toVecmath(in.getPos()));
-		//TODO Normals
-		// TRSRTransformation.toVecmath(matrix)
-		return in;
-	}
+    @Override
+    public DefaultUnpackedVertex pipe(DefaultUnpackedVertex in) {
+        matrix.transform(TRSRTransformation.toVecmath(new Vector3f((float) in.getPos().x, (float) in.getPos().y, (float) in.getPos().z)));
+        //TODO Normals
+        // TRSRTransformation.toVecmath(matrix)
+        return in;
+    }
 
 }
