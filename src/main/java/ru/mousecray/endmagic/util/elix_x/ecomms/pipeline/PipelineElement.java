1f4b6aaa20d3742109c1d485aa924fe7c027cbe2
hohserg <hohserg1@yandex.ru>
2019-04-30 07:24:15 +0300

    Added elix utils for baked models

---
+++ src/main/java/ru/mousecray/endmagic/util/elix_x/ecomms/pipeline/PipelineElement.java
@@ -0,0 +1,31 @@
+package ru.mousecray.endmagic.util.elix_x.ecomms.pipeline;
+
+import java.util.function.Function;
+
+/**
+ * A basic element of a pipeline. By itself it acts as a function (and can be used as one). But it is intended to be used in a {@linkplain Pipeline}.
+ * 
+ * @author elix_x
+ *
+ * @param <I>
+ *            Input
+ * @param <O>
+ *            Output
+ */
+public interface PipelineElement<I, O> extends Function<I, O> {
+
+	/**
+	 * Pipes the input through this element resulting in output. Action is equivalent to {@link Function#apply(Object)};
+	 * 
+	 * @param in
+	 *            - input
+	 * @return output
+	 */
+	public O pipe(I in);
+
+	@Override
+	default O apply(I in){
+		return pipe(in);
+	}
+
+}
