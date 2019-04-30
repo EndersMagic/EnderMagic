b18782616ce037079dec06413f6adc9520666aca
hohserg <hohserg1@yandex.ru>
2019-04-30 16:49:58 +0300

    Unique key gor compass model

--- src/main/java/ru/mousecray/endmagic/items/EnderCompass.java
+++ src/main/java/ru/mousecray/endmagic/items/EnderCompass.java
@@ -5,32 +5,35 @@ import net.minecraft.entity.Entity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.client.model.ModelLoader;
 import ru.mousecray.endmagic.client.render.model.IModelRegistration;
 import ru.mousecray.endmagic.client.render.model.baked.ModelEnderCompass;
 import ru.mousecray.endmagic.util.IEMModel;
 
+import java.util.UUID;
+
 public class EnderCompass extends Item implements IEMModel {
-    public static double getAngle(ItemStack stack) {
-        return stack.getTagCompound() != null ? stack.getTagCompound().getDouble("targetAngle") : 0;
+    public boolean shouldCauseReequipAnimation(ItemStack oldStack, ItemStack newStack, boolean slotChanged) {
+        return false;
+    }
+
+    public static String getCompassKey(ItemStack stack) {
+        checkAndSetKey(stack);
+        return stack.getTagCompound().getString("compassKey");
+    }
+
+    private static void checkAndSetKey(ItemStack stack) {
+        if (stack.getTagCompound() == null)
+            stack.setTagCompound(new NBTTagCompound());
+
+        if (!stack.getTagCompound().hasKey("compassKey"))
+            stack.getTagCompound().setString("compassKey", UUID.randomUUID().toString());
     }
 
     @Override
     public void onUpdate(ItemStack stack, World worldIn, Entity entityIn, int itemSlot, boolean isSelected) {
-        setAngle(stack, calcAngle(entityIn.getPosition(), new BlockPos(0, 0, 0)));
-    }
-
-    private void setAngle(ItemStack stack, double angle) {
-        if (stack.getTagCompound() == null)
-            stack.setTagCompound(new NBTTagCompound());
-
-        stack.getTagCompound().setDouble("targetAngle", angle);
-    }
-
-    private double calcAngle(BlockPos pos1, BlockPos pos) {
-        return Math.atan2(pos1.getX() - pos.getX(), pos1.getZ() - pos.getZ());
+        checkAndSetKey(stack);
     }
 
     @Override
