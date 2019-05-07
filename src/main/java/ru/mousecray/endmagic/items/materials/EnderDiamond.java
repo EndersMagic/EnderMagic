070aa6db3d4ff66e71362560f0fb691b95a68b6e
hohserg <hohserg1@yandex.ru>
2019-05-07 16:57:04 +0300

    Correct resource name

--- src/main/java/ru/mousecray/endmagic/items/materials/EnderDiamond.java
+++ src/main/java/ru/mousecray/endmagic/items/materials/EnderDiamond.java
@@ -1,10 +1,11 @@
 package ru.mousecray.endmagic.items.materials;
 
 import com.google.common.collect.ImmutableMap;
+import ru.mousecray.endmagic.EM;
 import ru.mousecray.endmagic.items.ItemNamed;
 
 public class EnderDiamond extends ItemNamed {
     public EnderDiamond(String name, int color) {
-        super(name, ImmutableMap.of("items/colorless_diamond", color));
+        super(name, ImmutableMap.of(EM.ID + ":items/colorless_diamond", color));
     }
 }
--- src/main/java/ru/mousecray/endmagic/items/materials/EnderSteel.java
+++ src/main/java/ru/mousecray/endmagic/items/materials/EnderSteel.java
@@ -1,10 +1,11 @@
 package ru.mousecray.endmagic.items.materials;
 
 import com.google.common.collect.ImmutableMap;
+import ru.mousecray.endmagic.EM;
 import ru.mousecray.endmagic.items.ItemNamed;
 
 public class EnderSteel extends ItemNamed {
     public EnderSteel(String name, int color) {
-        super(name, ImmutableMap.of("items/colorless_steel", color));
+        super(name, ImmutableMap.of(EM.ID + ":items/colorless_steel", color));
     }
 }
