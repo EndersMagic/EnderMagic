f7fbe6be49867cb2d1bd4306252612a2bc47c417
hohserg <hohserg1@yandex.ru>
2019-05-07 16:56:34 +0300

    Corrected format

--- src/main/java/ru/mousecray/endmagic/items/materials/EnderCoal.java
+++ src/main/java/ru/mousecray/endmagic/items/materials/EnderCoal.java
@@ -1,10 +1,11 @@
 package ru.mousecray.endmagic.items.materials;
 
 import com.google.common.collect.ImmutableMap;
+import ru.mousecray.endmagic.EM;
 import ru.mousecray.endmagic.items.ItemNamed;
 
 public class EnderCoal extends ItemNamed {
     public EnderCoal(String name, int color) {
-        super(name, ImmutableMap.of("items/colorless_coal", color));
+        super(name, ImmutableMap.of(EM.ID + ":items/colorless_coal", color | 0xff000000));
     }
 }
