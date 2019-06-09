44f628f7c568fbd338f86fb1a583513205b2f8f8
hohserg <hohserg1@yandex.ru>
2019-06-09 21:15:42 +0300

    Added enum for trees

---
+++ src/main/java/ru/mousecray/endmagic/blocks/trees/EnderTreeType.java
@@ -0,0 +1,24 @@
+package ru.mousecray.endmagic.blocks.trees;
+
+import net.minecraft.block.material.MapColor;
+import net.minecraft.util.IStringSerializable;
+
+public enum EnderTreeType implements IStringSerializable {
+    DRAGON("dragon", MapColor.PURPLE),
+    NATURAL("natural", MapColor.BLUE),
+    IMMORTAL("immortal", MapColor.EMERALD),
+    VANISHING("vanishing", MapColor.SILVER);
+
+    private final String name;
+    private final MapColor mapColor;
+
+    EnderTreeType(String name, MapColor mapColor) {
+        this.name = name;
+        this.mapColor = mapColor;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+}
