784ec4cacc068f6f760b86508a8609fdc0e4b89b
mousecray <40426574+mousecray@users.noreply.github.com>
2019-06-09 16:42:41 +0300

    Update BlockCurseBush.java

--- src/main/java/ru/mousecray/endmagic/blocks/BlockCurseBush.java
+++ src/main/java/ru/mousecray/endmagic/blocks/BlockCurseBush.java
@@ -1,5 +1,29 @@
 package ru.mousecray.endmagic.blocks;
 
-public class BlockCurseBush {
+import java.util.Random;
 
+import net.minecraft.block.BlockBush;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityAreaEffectCloud;import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public class BlockCurseBush extends BlockBush {
+	
+	@Override
+	public void updateTick(World world, BlockPos pos, IBlockState state, Random rand) {
+		super.updateTick(world, pos, state, rand);
+		if(rand.nextInt(100) < 10) {
+			EntityAreaEffectCloud entity = new EntityAreaEffectCloud(world);
+//			entity.addEffect(new PotionEffect());
+		}
+	}
+	
+	@Override
+	public void onEntityCollidedWithBlock(World world, BlockPos pos, IBlockState state, Entity entity) {
+		super.onEntityCollidedWithBlock(world, pos, state, entity);
+	}
 }
