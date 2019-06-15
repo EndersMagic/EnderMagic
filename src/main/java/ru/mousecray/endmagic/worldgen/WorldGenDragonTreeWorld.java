99e1d4b0d40a6053cae0bd0ba5ece0f499db1da9
hohserg <hohserg1@yandex.ru>
2019-06-15 04:47:38 +0300

    Remove all, and Added test glass worldgen

--- src/main/java/ru/mousecray/endmagic/worldgen/WorldGenDragonTreeWorld.java
+++ src/main/java/ru/mousecray/endmagic/worldgen/WorldGenDragonTreeWorld.java
@@ -1,16 +1,14 @@
 package ru.mousecray.endmagic.worldgen;
 
+import net.minecraft.init.Blocks;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraft.world.gen.IChunkGenerator;
-import org.apache.commons.lang3.tuple.Pair;
 
-import java.util.Arrays;
 import java.util.Random;
-import java.util.function.Function;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import static net.minecraft.init.Blocks.END_STONE;
@@ -18,65 +16,31 @@ import static net.minecraft.init.Blocks.END_STONE;
 public class WorldGenDragonTreeWorld {
     private WorldGenDragonTree generator = new WorldGenDragonTree(true);
 
-    private int centralIslandSize = 30;
+    private int centralIslandSize = 9;
+    //3;minecraft:bedrock,2*minecraft:dirt,minecraft:end_portal;1;village
 
     public void generateWorld(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider) {
+
         if (chunkX * chunkX + chunkZ * chunkZ < centralIslandSize * centralIslandSize) {
             if (!world.getChunkFromChunkCoords(chunkX, chunkZ).isEmpty()) {
-                findVerticalSlope(world, chunkX, chunkZ).forEach(place -> generator.generate(world, random, place));
+                BlockPos.MutableBlockPos pos = new BlockPos.MutableBlockPos();
+                int startX = chunkX << 4;
+                int startZ = chunkZ << 4;
+                Chunk chunk = world.getChunkFromChunkCoords(chunkX, chunkZ);
+                for (int x = startX; x < startX + 16; x++) {
+                    for (int z = startZ; z < startZ + 16; z++) {
+                        for (int y = 30; y < 50; y++) {
+                            pos.setPos(x, y, z);
+                            if (chunk.getBlockState(pos).getBlock().isAir(chunk.getBlockState(pos), world, pos))
+                                chunk.setBlockState(pos, Blocks.GLASS.getDefaultState());
+                        }
+                    }
+                }
             }
         }
     }
 
-    private Stream<BlockPos> findVerticalSlope(World world, int chunkX, int chunkZ) {
-        int ymin = 30;
-        int ymax = 50;
 
-        return IntStream.rangeClosed(ymin, ymax).mapToObj(y ->
-                IntStream.rangeClosed(5, 11).mapToObj(x ->
-                        IntStream.rangeClosed(5, 11).mapToObj(z ->
-                                new BlockPos(x + (chunkX << 4), y, z + (chunkZ << 4))))
-                        .flatMap(Function.identity())
-                        .filter(world::isAirBlock).flatMap(startPos ->
-                        {
-                            Stream<BlockPos> blockPosStream = Arrays.stream(EnumFacing.HORIZONTALS)
-                                    .flatMap(side -> walk(world, startPos.toImmutable(), side))
-                                    .filter(pos -> checkFreePlane(world, pos.getLeft(), pos.getRight()))
-                                    .flatMap(i ->
-                                            Stream.of(
-                                                    i.getLeft().offset(i.getRight())
-                                            )
-                                    );
-                            return blockPosStream;
-                        }
-                )
-        ).flatMap(Function.identity());
-    }
-
-    private boolean checkFreePlane(World world, BlockPos pos, EnumFacing air) {
-        EnumFacing side = air.rotateY();
-        return IntStream.rangeClosed(-1, 1).mapToObj(i ->
-                IntStream.rangeClosed(-2, 2).mapToObj(j -> {
-                    BlockPos current = pos.offset(side, i).offset(EnumFacing.UP, j);
-                    return world.getBlockState(current).getBlock() != END_STONE || !world.isAirBlock(current.offset(air));
-                })
-        ).flatMap(Function.identity())
-                .noneMatch(i -> i);
-    }
-
-    public static Stream<Pair<BlockPos, EnumFacing>> walk(World world, BlockPos start, EnumFacing to) {
-        int maxX = (start.getX() >> 4 << 4) + 16;
-        int maxZ = (start.getZ() >> 4 << 4) + 16;
-
-        BlockPos current = start;
-        while (world.getBlockState(current).getBlock() != END_STONE)
-            if (current.getX() < maxX && current.getZ() < maxZ)
-                current = current.offset(to);
-            else
-                return Stream.empty();
-
-        return Stream.of(Pair.of(current, to.getOpposite()));
-    }
     public static Stream<BlockPos> walkAround(World world, BlockPos current, EnumFacing dirrection, EnumFacing up, int step) {
         return Stream.iterate(new TraverseState(current, dirrection, up, 0),
                 state -> {
