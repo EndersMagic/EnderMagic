10d6a8644e42c0727e3398bb7bd11a1dc106c23b
hohserg <hohserg1@yandex.ru>
2019-06-30 19:21:00 +0300

    Make bounds more free
    No only world can be environment

--- src/main/java/ru/mousecray/endmagic/worldgen/monad/WorldIO.java
+++ src/main/java/ru/mousecray/endmagic/worldgen/monad/WorldIO.java
@@ -6,18 +6,18 @@ import net.minecraft.util.math.BlockPos;
 import java.util.Collection;
 import java.util.function.Function;
 
-public interface WorldIO<E extends OnlyWorld, T> {
+public interface WorldIO<E, T> {
     T performIO(E world);
 
-    static <T1> WorldIO<OnlyWorld, T1> lift(T1 value) {
+    static <T1> WorldIO<Object, T1> lift(T1 value) {
         return __ -> value;
     }
 
-    static <E1 extends OnlyWorld> WorldIO<E1, Void> start() {
+    static <E1> WorldIO<E1, Void> start() {
         return (WorldIO<E1, Void>) none;
     }
 
-    static WorldIO<OnlyWorld, Void> none = __ -> null;
+    static WorldIO<Object, Void> none = __ -> null;
 
     default <T1> WorldIO<E, T1> flatMap(Function<T, WorldIO<? super E, T1>> f) {
         return (E world) -> f.apply(performIO(world)).performIO(world);
@@ -31,7 +31,7 @@ public interface WorldIO<E extends OnlyWorld, T> {
         return flatMap(__ -> second);
     }
 
-    static <E extends OnlyWorld, A1> WorldIO<E, A1> sequence(Collection<WorldIO<E, A1>> seq) {
+    static <E, A1> WorldIO<E, A1> sequence(Collection<WorldIO<E, A1>> seq) {
         return seq.stream().reduce(WorldIO::andThen).orElse((WorldIO<E, A1>) none);
     }
 
