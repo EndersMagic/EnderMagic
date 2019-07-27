b594880e701b20556a16fb5e91d5a3830a20e327
hohserg <hohserg1@yandex.ru>
2019-07-27 14:44:14 +0300

    Remove redundant

--- src/main/java/ru/mousecray/endmagic/init/EMEvents.java
+++ src/main/java/ru/mousecray/endmagic/init/EMEvents.java
@@ -42,32 +42,6 @@ import java.util.stream.StreamSupport;
 @EventBusSubscriber(modid = EM.ID)
 public class EMEvents {
 
-    static <T> Spliterator<T> takeWhile(
-            Spliterator<T> splitr, Predicate<? super T> predicate) {
-        return new Spliterators.AbstractSpliterator<T>(splitr.estimateSize(), 0) {
-            boolean stillGoing = true;
-
-            @Override
-            public boolean tryAdvance(Consumer<? super T> consumer) {
-                if (stillGoing) {
-                    boolean hadNext = splitr.tryAdvance(elem -> {
-                        if (predicate.test(elem)) {
-                            consumer.accept(elem);
-                        } else {
-                            stillGoing = false;
-                        }
-                    });
-                    return hadNext && stillGoing;
-                }
-                return false;
-            }
-        };
-    }
-
-    static <T> Stream<T> takeWhile(Stream<T> stream, Predicate<? super T> predicate) {
-        return StreamSupport.stream(takeWhile(stream.spliterator(), predicate), false);
-    }
-
     @SubscribeEvent
     public static void onPlayerEnter(EntityJoinWorldEvent event) {
         if (!event.getWorld().isRemote)
