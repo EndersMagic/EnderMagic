bedc20123728e1d77bfbe75a2e28ffbc83c471ba
hohserg <hohserg1@yandex.ru>
2020-02-19 15:40:25 +0300

    Reformat imports

--- src/main/java/ru/mousecray/endmagic/worldgen/trees/world/WorldGenPhantomTreeWorld.java
+++ src/main/java/ru/mousecray/endmagic/worldgen/trees/world/WorldGenPhantomTreeWorld.java
@@ -1,11 +1,11 @@
 package ru.mousecray.endmagic.worldgen.trees.world;
 
-import java.util.Random;
-
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.Chunk;
 import ru.mousecray.endmagic.worldgen.trees.WorldGenPhantomTree;
+
+import java.util.Random;
 
 import static ru.mousecray.endmagic.worldgen.trees.world.WorldGenDragonTreeWorld.centralIslandSize;
 
