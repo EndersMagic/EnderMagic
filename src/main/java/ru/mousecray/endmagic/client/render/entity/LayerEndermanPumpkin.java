54d89b96a4952954c65de60c4c3e82945d3fa934
hohserg <hohserg1@yandex.ru>
2020-03-07 19:13:21 +0300

    Added to layer dependency of capa

--- src/main/java/ru/mousecray/endmagic/client/render/entity/LayerEndermanPumpkin.java
+++ src/main/java/ru/mousecray/endmagic/client/render/entity/LayerEndermanPumpkin.java
@@ -1,5 +1,6 @@
 package ru.mousecray.endmagic.client.render.entity;
 
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.block.model.ItemCameraTransforms;
@@ -10,6 +11,7 @@ import net.minecraft.entity.monster.EntityEnderman;
 import net.minecraft.entity.monster.EntitySnowman;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
+import ru.mousecray.endmagic.capability.entity.EndermanHeadBlockCapabilityProvider;
 
 public class LayerEndermanPumpkin implements LayerRenderer<EntityEnderman> {
     private final RenderSnowMan snowManRenderer = (RenderSnowMan) Minecraft.getMinecraft().getRenderManager().<EntitySnowman>getEntityClassRenderObject(EntitySnowman.class);
@@ -21,7 +23,8 @@ public class LayerEndermanPumpkin implements LayerRenderer<EntityEnderman> {
 
     @Override
     public void doRenderLayer(EntityEnderman entitylivingbaseIn, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, float scale) {
-        if (!entitylivingbaseIn.isInvisible() /*&& entitylivingbaseIn.isPumpkinEquipped()*/) {
+        IBlockState block = entitylivingbaseIn.getCapability(EndermanHeadBlockCapabilityProvider.endermanHeadBlockCapability, null).block;
+        if (!entitylivingbaseIn.isInvisible() && block.getBlock() != Blocks.AIR) {
             //snowManRenderer.getMainModel().head.rotateAngleY = netHeadYaw * 0.017453292F;
             //snowManRenderer.getMainModel().head.rotateAngleX = headPitch * 0.017453292F;
 
@@ -29,13 +32,12 @@ public class LayerEndermanPumpkin implements LayerRenderer<EntityEnderman> {
             renderEnderman.getMainModel().bipedHead.postRender(0.0625F);
 
 
-
             snowManRenderer.getMainModel().head.postRender(0.125F);
             float f = 0.625F;
             GlStateManager.scale(0.625F, -0.625F, -0.625F);
-            GlStateManager.rotate(180, 0,1,0);
-            GlStateManager.translate( 0,1.41,0);
-            Minecraft.getMinecraft().getItemRenderer().renderItem(entitylivingbaseIn, new ItemStack(Blocks.PUMPKIN, 1), ItemCameraTransforms.TransformType.HEAD);
+            GlStateManager.rotate(180, 0, 1, 0);
+            GlStateManager.translate(0, 1.41, 0);
+            Minecraft.getMinecraft().getItemRenderer().renderItem(entitylivingbaseIn, new ItemStack(block.getBlock(), 1), ItemCameraTransforms.TransformType.HEAD);
             GlStateManager.popMatrix();
         }
     }
