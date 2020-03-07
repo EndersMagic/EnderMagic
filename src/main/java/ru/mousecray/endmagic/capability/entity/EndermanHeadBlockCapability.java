150f57b0b7bc3584a07e62ead7c0f4861519c651
hohserg <hohserg1@yandex.ru>
2020-03-07 19:12:35 +0300

    Added default value

--- src/main/java/ru/mousecray/endmagic/capability/entity/EndermanHeadBlockCapability.java
+++ src/main/java/ru/mousecray/endmagic/capability/entity/EndermanHeadBlockCapability.java
@@ -1,7 +1,8 @@
 package ru.mousecray.endmagic.capability.entity;
 
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
 
 public class EndermanHeadBlockCapability {
-    public IBlockState block;
+    public IBlockState block = Blocks.AIR.getDefaultState();
 }
