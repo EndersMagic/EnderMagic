c5cbadfea26d9e74bff037009d543b9c641fa215
mousecray <40426574+mousecray@users.noreply.github.com>
2020-03-11 15:48:33 +0300

    Create counter for entity id and change update frequency

--- src/main/java/ru/mousecray/endmagic/init/EMEntities.java
+++ src/main/java/ru/mousecray/endmagic/init/EMEntities.java
@@ -5,25 +5,26 @@ import net.minecraftforge.fml.common.registry.EntityEntryBuilder;
 import ru.mousecray.endmagic.entity.*;
 
 public class EMEntities {
+	private static int nextId = 0;
 	public static EntityEntry emEnderPearl = EntityEntryBuilder
 			.create()
 			.entity(EntityEMEnderPearl.class)
 			.name("EMEnderPearl")
-			.id("em_ender_pearl", 0)
-			.tracker(64, 20, true)
+			.id("em_ender_pearl", nextId++)
+			.tracker(64, 50, true)
 			.build();
 	public static EntityEntry enderArrow = EntityEntryBuilder
 			.create()
 			.entity(EntityEnderArrow.class)
 			.name("EnderArrow")
-			.id("ender_arrow", 1)
+			.id("ender_arrow", nextId++)
 			.tracker(64, 20, true)
 			.build();
 	public static EntityEntry entityCurseBush = EntityEntryBuilder
 			.create()
 			.entity(EntityCurseBush.class)
 			.name("CurseBush")
-			.id("curse_bush", 2)
+			.id("curse_bush", nextId++)
 			.tracker(32, 1, true)
 			.build();
 }
