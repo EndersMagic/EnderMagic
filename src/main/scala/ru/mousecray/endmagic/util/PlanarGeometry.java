2997c0e64472249f9dbede9dad85ddee52e57444
hohserg <hohserg1@yandex.ru>
2020-03-14 17:11:36 +0300

    Correct x/y planar coods

--- src/main/scala/ru/mousecray/endmagic/util/PlanarGeometry.java
+++ src/main/scala/ru/mousecray/endmagic/util/PlanarGeometry.java
@@ -6,19 +6,22 @@ import net.minecraft.util.math.Vec3i;
 public class PlanarGeometry {
 
     public static Vec2i projectTo2d(Vec3i vertex, EnumFacing facing) {
+        int x = vertex.getX();
+        int z = vertex.getZ();
+        int y = vertex.getY();
         switch (facing) {
             case UP:
-                return new Vec2i(vertex.getX(), vertex.getZ());
+                return new Vec2i(z, x);
             case DOWN:
-                return new Vec2i(vertex.getX(), 15 - vertex.getZ());
+                return new Vec2i(15 - z,x);
             case EAST:
-                return new Vec2i(15 - vertex.getZ(), 15 - vertex.getY());
+                return new Vec2i( 15 - y,15 - z);
             case WEST:
-                return new Vec2i(vertex.getZ(), 15 - vertex.getY());
+                return new Vec2i(15 - y,z);
             case NORTH:
-                return new Vec2i(15 - vertex.getX(), 15 - vertex.getY());
+                return new Vec2i(15 - y,15 - x);
             case SOUTH:
-                return new Vec2i(vertex.getX(), 15 - vertex.getY());
+                return new Vec2i(15 - y,x);
             default:
                 throw new UnsupportedOperationException("no more facings");
         }
