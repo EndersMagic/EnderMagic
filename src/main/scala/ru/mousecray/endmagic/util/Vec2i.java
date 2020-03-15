b182ca47ed40fb4d074468731d99ff2e292d31cd
hohserg <hohserg1@yandex.ru>
2020-03-15 13:52:09 +0300

    Fix capability for hash map key

--- src/main/scala/ru/mousecray/endmagic/util/Vec2i.java
+++ src/main/scala/ru/mousecray/endmagic/util/Vec2i.java
@@ -1,7 +1,9 @@
 package ru.mousecray.endmagic.util;
 
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
 public class Vec2i {
-    public final int x,y;
+    public final int x, y;
 
     public Vec2i(int x, int y) {
         this.x = x;
@@ -10,6 +12,19 @@ public class Vec2i {
 
     @Override
     public String toString() {
-        return "Vec2i(x = "+x+", y = "+y+")";
+        return "Vec2i(x = " + x + ", y = " + y + ")";
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder().append(x).append(y).toHashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Vec2i) {
+            return ((Vec2i) obj).x == x && ((Vec2i) obj).y == y;
+        } else
+            return false;
     }
 }
