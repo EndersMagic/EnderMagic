32b4ce6d2bdae88ecefd358766d7d096e47106e7
mousecray <40426574+mousecray@users.noreply.github.com>
2020-03-17 15:39:14 +0300

    Added SideOnly annotation to @EMEntity

--- src/main/java/ru/mousecray/endmagic/util/registry/EMEntity.java
+++ src/main/java/ru/mousecray/endmagic/util/registry/EMEntity.java
@@ -2,6 +2,8 @@ package ru.mousecray.endmagic.util.registry;
 
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.entity.Entity;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -15,6 +17,7 @@ import java.lang.annotation.Target;
  *     super(manager)
  * }
  */
+@SideOnly(Side.CLIENT)
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface EMEntity {
