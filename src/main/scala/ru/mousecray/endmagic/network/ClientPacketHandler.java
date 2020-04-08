e183a26c2d4fc0a0215da24cf8a5005343c51dea
hohserg <hohserg1@yandex.ru>
2020-04-08 20:18:47 +0300

    Added REMOVE_RUNE_STATE and use code blocks for separate cases

--- src/main/scala/ru/mousecray/endmagic/network/ClientPacketHandler.java
+++ src/main/scala/ru/mousecray/endmagic/network/ClientPacketHandler.java
@@ -10,12 +10,13 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.chunk.Chunk;
 import ru.mousecray.endmagic.EM;
 import ru.mousecray.endmagic.capability.chunk.IRuneChunkCapability;
-import ru.mousecray.endmagic.capability.chunk.RuneStateCapabilityProvider;
+import ru.mousecray.endmagic.capability.chunk.RunePart;
 import ru.mousecray.endmagic.capability.world.PhantomAvoidingGroup;
 import ru.mousecray.endmagic.capability.world.PhantomAvoidingGroupCapability;
 import ru.mousecray.endmagic.capability.world.PhantomAvoidingGroupCapabilityProvider;
 import ru.mousecray.endmagic.client.render.model.baked.FinalisedModelEnderCompass;
 import ru.mousecray.endmagic.rune.RuneIndex;
+import ru.mousecray.endmagic.util.Vec2i;
 
 import static ru.mousecray.endmagic.capability.chunk.RuneStateCapabilityProvider.*;
 
@@ -24,10 +25,11 @@ public class ClientPacketHandler implements ICustomPacketHandler.IClientPacketHa
     @Override
     public void handlePacket(PacketCustom packetCustom, Minecraft minecraft, INetHandlerPlayClient iNetHandlerPlayClient) {
         switch (PacketTypes.valueOf(packetCustom.getType())) {
-            case UPDATE_COMPAS_TARGET:
+            case UPDATE_COMPAS_TARGET: {
                 FinalisedModelEnderCompass.target.put(packetCustom.readInt(), packetCustom.readPos());
                 break;
-            case UPDATE_PHANROM_AVOIDINCAPABILITY:
+            }
+            case UPDATE_PHANROM_AVOIDINCAPABILITY: {
                 WorldClient world = Minecraft.getMinecraft().world;
                 if (packetCustom.readInt() == world.provider.getDimension()) {
                     PhantomAvoidingGroupCapability capability = world.getCapability(PhantomAvoidingGroupCapabilityProvider.avoidingGroupCapability, null);
@@ -46,12 +48,23 @@ public class ClientPacketHandler implements ICustomPacketHandler.IClientPacketHa
                     }
                 }
                 break;
-            case SYNC_RUNE_CAPABILITY:
+            }
+            case SYNC_RUNE_CAPABILITY: {
                 Chunk chunk = minecraft.world.getChunkFromChunkCoords(packetCustom.readInt(), packetCustom.readInt());
                 IRuneChunkCapability capability = RuneIndex.getCapability(chunk);
                 runeStateCapability.readNBT(capability, EnumFacing.UP, packetCustom.readNBTTagCompound());
-                EM.proxy.refreshChunk(capability.existingRunes().isEmpty()?new BlockPos(chunk.x<<4,0,chunk.z<<4):capability.existingRunes().keySet().iterator().next());
+                EM.proxy.refreshChunk(minecraft.world, capability.existingRunes().isEmpty() ? new BlockPos(chunk.x << 4, 0, chunk.z << 4) : capability.existingRunes().keySet().iterator().next());
+
                 break;
+            }
+            case ADDED_RUNE_PART: {
+                RuneIndex.addRunePart(minecraft.world, packetCustom.readPos(), packetCustom.readEnumFacing(), new Vec2i(packetCustom.readInt(), packetCustom.readInt()), new RunePart());
+                break;
+            }
+            case REMOVE_RUNE_STATE:{
+                RuneIndex.removeRune(minecraft.world,packetCustom.readPos());
+                break;
+            }
             default:
                 break;
         }
