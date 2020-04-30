42d6af3c1ceef618e844848dabdf6cc566693408
mousecray <40426574+mousecray@users.noreply.github.com>
2020-04-30 12:57:27 +0300

    Update EnderArrow.java

--- src/main/scala/ru/mousecray/endmagic/items/EnderArrow.java
+++ src/main/scala/ru/mousecray/endmagic/items/EnderArrow.java
@@ -11,16 +11,11 @@ import net.minecraft.world.World;
 import ru.mousecray.endmagic.entity.EntityEnderArrow;
 
 public class EnderArrow extends ItemArrow implements ItemOneWhiteEMTextured {
-	
-	public EntityEnderArrow createArrow(World world, EntityLivingBase shooter) {
-		EntityEnderArrow arrow = new EntityEnderArrow(world, shooter);
-		return arrow;
-	}
 
 	@Override
 	public boolean isInfinite(ItemStack stack, ItemStack bow, EntityPlayer player) {
 		int enchant = EnchantmentHelper.getEnchantmentLevel(Enchantments.INFINITY, bow);
-		return enchant <= 0 ? false : this.getClass() == EnderArrow.class;
+		return enchant > 0 && this.getClass() == EnderArrow.class;
 	}
 
 	@Override
