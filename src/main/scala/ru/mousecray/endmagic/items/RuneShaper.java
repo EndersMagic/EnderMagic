a61c97f80906111b72d390d6827dae23e136bafe
hohserg <hohserg1@yandex.ru>
2020-05-11 19:01:11 +0300

    Added test item

---
+++ src/main/scala/ru/mousecray/endmagic/items/RuneShaper.java
@@ -0,0 +1,23 @@
+package ru.mousecray.endmagic.items;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import ru.mousecray.endmagic.capability.chunk.Rune;
+import ru.mousecray.endmagic.rune.RuneIndex;
+
+public class RuneShaper extends Item {
+    @Override
+    public EnumActionResult onItemUse(EntityPlayer player, World world, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+        RuneIndex.getRune(world, pos, facing).ifPresent(this::println);
+        return EnumActionResult.SUCCESS;
+    }
+
+    private void println(Rune rune) {
+        System.out.println(rune.parts());
+    }
+}
