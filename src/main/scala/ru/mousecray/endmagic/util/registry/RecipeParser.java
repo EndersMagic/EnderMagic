fa49ffa1dda0039a680b90862d90903ac57d801b
hohserg <hohserg1@yandex.ru>
2020-05-19 14:23:11 +0300

    Added recipe parser

---
+++ src/main/scala/ru/mousecray/endmagic/util/registry/RecipeParser.java
@@ -0,0 +1,154 @@
+package ru.mousecray.endmagic.util.registry;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.IRecipe;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.crafting.ShapedRecipes;
+import net.minecraft.item.crafting.ShapelessRecipes;
+import net.minecraft.util.NonNullList;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.common.registry.ForgeRegistries;
+import scala.collection.immutable.StringOps;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toMap;
+
+public class RecipeParser {
+    public static List<IRecipe> parse(String fileContent) {
+        Map<String, List<String>> sections = parseSections(fileContent);
+        Map<Character, ItemStack> id_map = sections.getOrDefault("id_map", ImmutableList.of()).stream()
+                .map(i -> split(i, '|'))
+                .filter(i -> i.length == 2)
+                .collect(toMap(i -> i[0].toCharArray()[0], i -> findItem(i[1])));
+
+        checkInvalidSymbols(id_map);
+
+        List<IRecipe> recipes = sections.getOrDefault("recipes", ImmutableList.of()).stream()
+                .map(RecipeParser::removeSpaces)
+                .map(i -> split(i, '|'))
+                .filter(i -> i.length == 2)
+                .map(i -> {
+                    ItemStack result = findItem(i[0]);
+                    int leftBracket = i[1].indexOf('(');
+                    int rightBracket = i[1].indexOf(')');
+
+                    String recipeType = i[1].substring(0, leftBracket);
+
+                    String recipe = i[1].substring(leftBracket+1, rightBracket);
+
+                    NonNullList<Ingredient> ingredients = recipe.replaceAll(",", "").chars()
+                            .mapToObj(c -> id_map.getOrDefault((char) c, ItemStack.EMPTY))
+                            .map(Ingredient::fromStacks)
+                            .collect(Collectors.toCollection(NonNullList::create));
+
+                    if(recipeType.equals("shaped")){
+                        String[] recipeLines = split(recipe, ',');
+                        if(recipeLines.length==0)
+                            throw new IllegalArgumentException("Invalid recipe "+i[0]+"| "+i[1]);
+
+                        return new ShapedRecipes("",recipeLines[0].length(),recipeLines.length,ingredients,result);
+                    }else if(recipeType.equals("shapeless")){
+                        return new ShapelessRecipes("",result,
+                                ingredients);
+                    }
+                    return null;
+                }).collect(toList());
+
+        return recipes;
+    }
+
+    private static void checkInvalidSymbols(Map<Character, ItemStack> id_map) {
+
+    }
+
+    private static ItemStack findItem(String id) {
+        String[] domian_name_meta = split(id, ':');
+        if (domian_name_meta.length < 2)
+            throw new IllegalArgumentException("invalid item id: " + id);
+
+        String domian = domian_name_meta[0];
+        String name = domian_name_meta[1];
+        int meta = domian_name_meta.length==3?Integer.parseInt(domian_name_meta[2]):0;
+
+        return ItemStack.EMPTY;//new ItemStack(ForgeRegistries.ITEMS.getValue(new ResourceLocation(domian,name)),1,meta);
+
+    }
+
+    private static Map<String, List<String>> parseSections(String fileContent) {
+        ImmutableMap.Builder<String, List<String>> r = ImmutableMap.<String, List<String>>builder();
+
+        String[] lines = fileContent.split("\\r?\\n");
+
+        int i = 0;
+        while (i < lines.length) {
+            String line = lines[i];
+            if (line.isEmpty())
+                i++;
+            else {
+
+                int firstBracket = line.indexOf("{");
+
+                if (firstBracket == -1)
+                    throw incorrectLine(fileContent, i);
+
+                String name = removeSpaces(line.substring(0, firstBracket));
+
+                if (name.isEmpty())
+                    throw incorrectLine(fileContent, i);
+
+                int secondBracket = i + 1;
+                while (secondBracket < lines.length && !lines[secondBracket].contains("}"))
+                    secondBracket++;
+
+                if (!lines[secondBracket].contains("}"))
+                    throw incorrectLine(fileContent, i);
+
+                ImmutableList.Builder<String> list = ImmutableList.builder();
+
+                for (int j = i + 1; j < secondBracket; j++)
+                    list.add(split(lines[j], ';'));
+
+                r.put(name, list.build());
+                i = secondBracket + 1;
+            }
+        }
+
+        return r.build();
+    }
+
+    private static String removeSpaces(String some) {
+        return some.replaceAll("\\s+", "");
+    }
+
+    public static String[] split(String some, char separator) {
+        return new StringOps(some).split(separator);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("test");
+
+        List<IRecipe> sections = parse("id_map {\n" +
+                "  #| minecraft:apple\n" +
+                "  B| endmagic:blue_pearl;  G| custommod:customitem\n" +
+                "}\n" +
+                "recipes {\n" +
+                "  endmagic:tallgrass| shaped (###, B#G, ###)\n" +
+                "  custommod:customitem| shapeless (##G##G##,#)\n" +
+                "}");
+
+        //sections.forEach((k, v) -> System.out.println(k + " -> " + v));
+    }
+
+    private static IllegalArgumentException incorrectLine(String fileContent, int i) {
+        IllegalArgumentException exception = new IllegalArgumentException("fileContent hash incorrect section at line " + i + "\n" + fileContent);
+        exception.setStackTrace(Arrays.copyOfRange(exception.getStackTrace(), 1, exception.getStackTrace().length));
+        return exception;
+    }
+}
