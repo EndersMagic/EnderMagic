1b706b915cbba6487f727820616a312e9d4962a8
hohserg <hohserg1@yandex.ru>
2020-05-24 18:57:57 +0300

    Added opening test gui on shift+right click with test item

--- src/main/scala/ru/mousecray/endmagic/items/Test.java
+++ src/main/scala/ru/mousecray/endmagic/items/Test.java
@@ -1,5 +1,6 @@
 package ru.mousecray.endmagic.items;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -7,24 +8,24 @@ import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
 import net.minecraft.world.World;
-import ru.mousecray.endmagic.EM;
+import ru.mousecray.endmagic.client.gui.GuiScreenTest;
 
-public class Test extends Item
-{
-	
-	public Test() {
-		setMaxStackSize(1);
-		setUnlocalizedName("test");
-	}
+public class Test extends Item {
 
-	@Override
-	public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn)
-	{
-		if (playerIn.isSneaking())
-		{
-			if (playerIn.dimension == 0) playerIn.changeDimension(1);
-			else if (playerIn.dimension == 1) playerIn.changeDimension(0);
-		}
-		return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, playerIn.getHeldItem(handIn));
-	}
+    public Test() {
+        setMaxStackSize(1);
+        setUnlocalizedName("test");
+    }
+
+    @Override
+    public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) {
+        if (playerIn.isSneaking()) {
+            if (worldIn.isRemote)
+                Minecraft.getMinecraft().currentScreen = new GuiScreenTest();
+        } else {
+            if (playerIn.dimension == 0) playerIn.changeDimension(1);
+            else if (playerIn.dimension == 1) playerIn.changeDimension(0);
+        }
+        return new ActionResult<>(EnumActionResult.SUCCESS, playerIn.getHeldItem(handIn));
+    }
 }
