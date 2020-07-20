40a7840cce18b52d01287c786894acf6cac89f0a
hohserg <hohserg1@yandex.ru>
2020-07-21 01:34:32 +0300

    Added partial tick interpolation

--- src/main/scala/ru/mousecray/endmagic/client/render/entity/RenderEntityCustomEnderEye.java
+++ src/main/scala/ru/mousecray/endmagic/client/render/entity/RenderEntityCustomEnderEye.java
@@ -4,10 +4,16 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.renderer.entity.RenderSnowball;
 import net.minecraft.init.Items;
+import net.minecraft.util.math.BlockPos;
 import ru.mousecray.endmagic.entity.EntityCustomEnderEye;
 
 public class RenderEntityCustomEnderEye extends RenderSnowball<EntityCustomEnderEye> {
     public RenderEntityCustomEnderEye(RenderManager renderManagerIn) {
         super(renderManagerIn, Items.ENDER_EYE, Minecraft.getMinecraft().getRenderItem());
+    }
+
+    @Override
+    public void doRender(EntityCustomEnderEye entity, double x, double y, double z, float entityYaw, float partialTicks) {
+        super.doRender(entity, x + entity.motionX * partialTicks, y + entity.motionY * partialTicks, z + entity.motionZ * partialTicks, entityYaw, partialTicks);
     }
 }
