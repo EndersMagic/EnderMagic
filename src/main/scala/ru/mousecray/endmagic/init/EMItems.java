bc1ae940c5b9ae91abb0d5901fde77fdc94fcbed
hohserg <hohserg1@yandex.ru>
2020-07-26 20:09:36 +0300

    Added chainmail armor items

--- src/main/scala/ru/mousecray/endmagic/init/EMItems.java
+++ src/main/scala/ru/mousecray/endmagic/init/EMItems.java
@@ -69,10 +69,16 @@ public class EMItems {
                                     new EMAxe(((MaterialProvider) material).material(), material.getCustomName() + "_axe", toolColor),
                                     new EMSword(((MaterialProvider) material).material(), material.getCustomName() + "_sword", toolColor),
                                     new EMHoe(((MaterialProvider) material).material(), material.getCustomName() + "_hoe", toolColor),
+
                                     new EMArmor(((MaterialProvider) material).armorMaterial(), 4, EntityEquipmentSlot.HEAD,  "_helmet", material, toolColor),
                                     new EMArmor(((MaterialProvider) material).armorMaterial(), 4, EntityEquipmentSlot.CHEST,  "_chestplate", material, toolColor),
                                     new EMArmor(((MaterialProvider) material).armorMaterial(), 4, EntityEquipmentSlot.LEGS,  "_leggings", material, toolColor),
-                                    new EMArmor(((MaterialProvider) material).armorMaterial(), 4, EntityEquipmentSlot.FEET,  "_boots", material, toolColor)
+                                    new EMArmor(((MaterialProvider) material).armorMaterial(), 4, EntityEquipmentSlot.FEET,  "_boots", material, toolColor),
+
+                                    new EMArmor(((MaterialProvider) material).armorMaterial(), 4, EntityEquipmentSlot.HEAD,  "_helmet_chainmail", material, toolColor),
+                                    new EMArmor(((MaterialProvider) material).armorMaterial(), 4, EntityEquipmentSlot.CHEST,  "_chestplate_chainmail", material, toolColor),
+                                    new EMArmor(((MaterialProvider) material).armorMaterial(), 4, EntityEquipmentSlot.LEGS,  "_leggings_chainmail", material, toolColor),
+                                    new EMArmor(((MaterialProvider) material).armorMaterial(), 4, EntityEquipmentSlot.FEET,  "_boots_chainmail", material, toolColor)
                             );
                         }
                 ).collect(Collectors.toList());
