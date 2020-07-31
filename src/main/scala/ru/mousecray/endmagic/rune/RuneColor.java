d24b48aa2592cb3ae27cb8811d25eaf8aef39afe
hohserg <hohserg1@yandex.ru>
2020-07-31 14:04:15 +0300

    Added elements

--- src/main/scala/ru/mousecray/endmagic/rune/RuneColor.java
+++ src/main/scala/ru/mousecray/endmagic/rune/RuneColor.java
@@ -1,9 +1,21 @@
 package ru.mousecray.endmagic.rune;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.util.ResourceLocation;
+import ru.mousecray.endmagic.EM;
+
+import java.awt.*;
+
 public enum RuneColor {
-    white(255, 255, 255),
-    green(0, 255, 0)
-    ;
+    light(new Color(255, 255, 200)),
+    fire(new Color(255, 79, 0)),
+    water(new Color(50, 100, 255)),
+    earth(new Color(200, 170, 0)),
+    ender(new Color(27, 123, 107)),
+    air(new Color(200, 255, 200), new ResourceLocation(EM.ID, "runes/test")),
+    darkness(new Color(10, 10, 10), new ResourceLocation(EM.ID, "runes/test"));
+
 
     public final int r;
     public final int g;
