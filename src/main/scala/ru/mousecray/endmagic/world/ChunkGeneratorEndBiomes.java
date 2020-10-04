e2af0a55c0102f951ddd9ce5154b7c1d32dbb377
GoogeTan <61940455+GoogeTan@users.noreply.github.com>
2020-10-04 08:54:29 +0300

    Update ChunkGeneratorEndBiomes.java

--- src/main/scala/ru/mousecray/endmagic/world/ChunkGeneratorEndBiomes.java
+++ src/main/scala/ru/mousecray/endmagic/world/ChunkGeneratorEndBiomes.java
@@ -1,21 +1,31 @@
 package ru.mousecray.endmagic.world;
 
+import net.minecraft.block.BlockChorusFlower;
+import net.minecraft.block.BlockFalling;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Biomes;
 import net.minecraft.init.Blocks;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.tileentity.TileEntityEndGateway;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.ChunkPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.ChunkPrimer;
 import net.minecraft.world.gen.ChunkGeneratorEnd;
+import net.minecraft.world.gen.MapGenCaves;
+import net.minecraft.world.gen.NoiseGeneratorOctaves;
 import net.minecraft.world.gen.NoiseGeneratorSimplex;
+import net.minecraft.world.gen.feature.WorldGenEndGateway;
 import net.minecraft.world.gen.feature.WorldGenEndIsland;
 import net.minecraft.world.gen.structure.MapGenEndCity;
 import net.minecraftforge.event.ForgeEventFactory;
+import net.minecraftforge.event.terraingen.ChunkGeneratorEvent;
 import net.minecraftforge.event.terraingen.InitMapGenEvent;
+import net.minecraftforge.event.terraingen.InitNoiseGensEvent;
 import net.minecraftforge.event.terraingen.TerrainGen;
 
 import java.util.Random;
@@ -26,12 +36,21 @@ public class ChunkGeneratorEndBiomes extends ChunkGeneratorEnd
     private final Random rand;
     protected static final IBlockState END_STONE = Blocks.END_STONE.getDefaultState();
     protected static final IBlockState AIR = Blocks.AIR.getDefaultState();
+    private NoiseGeneratorOctaves lperlinNoise1;
+    private NoiseGeneratorOctaves lperlinNoise2;
+    private NoiseGeneratorOctaves perlinNoise1;
+    /** A NoiseGeneratorOctaves used in generating terrain */
+    public NoiseGeneratorOctaves noiseGen5;
+    /** A NoiseGeneratorOctaves used in generating terrain */
+    public NoiseGeneratorOctaves noiseGen6;
     /** Reference   to the World object. */
     private final World world;
     /** are map structures going to be generated (e.g. strongholds) */
     private final boolean mapFeaturesEnabled;
+    private final BlockPos spawnPoint;
     private MapGenEndCity endCityGen = new MapGenEndCity(this);
     private NoiseGeneratorSimplex islandNoise;
+    private double[] buffer;
     /** The biomes that are used to generate the chunk */
     private Biome[] biomesForGeneration;
     double[] pnr;
@@ -44,12 +63,99 @@ public class ChunkGeneratorEndBiomes extends ChunkGeneratorEnd
     	super(world, featuresEnabled, seed, spawnPos);
         this.world = world;
         this.mapFeaturesEnabled = featuresEnabled;
+        this.spawnPoint = spawnPos;
         this.rand = new Random(seed);
+        this.lperlinNoise1 = new NoiseGeneratorOctaves(this.rand, 16);
+        this.lperlinNoise2 = new NoiseGeneratorOctaves(this.rand, 16);
+        this.perlinNoise1 = new NoiseGeneratorOctaves(this.rand, 8);
+        this.noiseGen5 = new NoiseGeneratorOctaves(this.rand, 10);
+        this.noiseGen6 = new NoiseGeneratorOctaves(this.rand, 16);
         this.islandNoise = new NoiseGeneratorSimplex(this.rand);
+
+       InitNoiseGensEvent.ContextEnd ctx =
+                new InitNoiseGensEvent.ContextEnd(lperlinNoise1, lperlinNoise2, perlinNoise1, noiseGen5, noiseGen6, islandNoise);
+        ctx = TerrainGen.getModdedNoiseGenerators(world, this.rand, ctx);
+        this.lperlinNoise1 = ctx.getLPerlin1();
+        this.lperlinNoise2 = ctx.getLPerlin2();
+        this.perlinNoise1 = ctx.getPerlin();
+        this.noiseGen5 = ctx.getDepth();
+        this.noiseGen6 = ctx.getScale();
+        this.islandNoise = ctx.getIsland();
         this.endCityGen = (MapGenEndCity)  TerrainGen.getModdedMapGen(this.endCityGen, InitMapGenEvent.EventType.END_CITY);
     }
 
-    public void buildSurfaces(ChunkPrimer primer, int chunkX, int chunkZ, Biome[] biomeMap)//важно - есть изменения
+    /**
+     * Generates a bare-bones chunk of nothing but stone or ocean blocks, formed, but featureless.
+     */
+    public void setBlocksInChunk(int x, int z, ChunkPrimer primer)
+    {
+        int i = 2;
+        int j = 3;
+        int k = 33;
+        int l = 3;
+        this.buffer = this.getHeights(this.buffer, x * 2, 0, z * 2, 3, 33, 3);
+
+        for (int i1 = 0; i1 < 2; ++i1)
+        {
+            for (int j1 = 0; j1 < 2; ++j1)
+            {
+                for (int k1 = 0; k1 < 32; ++k1)
+                {
+                    double d0 = 0.25D;
+                    double d1 = this.buffer[((i1 + 0) * 3 + j1 + 0) * 33 + k1 + 0];
+                    double d2 = this.buffer[((i1 + 0) * 3 + j1 + 1) * 33 + k1 + 0];
+                    double d3 = this.buffer[((i1 + 1) * 3 + j1 + 0) * 33 + k1 + 0];
+                    double d4 = this.buffer[((i1 + 1) * 3 + j1 + 1) * 33 + k1 + 0];
+                    double d5 = (this.buffer[((i1 + 0) * 3 + j1 + 0) * 33 + k1 + 1] - d1) * 0.25D;
+                    double d6 = (this.buffer[((i1 + 0) * 3 + j1 + 1) * 33 + k1 + 1] - d2) * 0.25D;
+                    double d7 = (this.buffer[((i1 + 1) * 3 + j1 + 0) * 33 + k1 + 1] - d3) * 0.25D;
+                    double d8 = (this.buffer[((i1 + 1) * 3 + j1 + 1) * 33 + k1 + 1] - d4) * 0.25D;
+
+                    for (int l1 = 0; l1 < 4; ++l1)
+                    {
+                        double d9 = 0.125D;
+                        double d10 = d1;
+                        double d11 = d2;
+                        double d12 = (d3 - d1) * 0.125D;
+                        double d13 = (d4 - d2) * 0.125D;
+
+                        for (int i2 = 0; i2 < 8; ++i2)
+                        {
+                            double d14 = 0.125D;
+                            double d15 = d10;
+                            double d16 = (d11 - d10) * 0.125D;
+
+                            for (int j2 = 0; j2 < 8; ++j2)
+                            {
+                                IBlockState iblockstate = AIR;
+
+                                if (d15 > 0.0D)
+                                {
+                                    iblockstate = END_STONE;
+                                }
+
+                                int k2 = i2 + i1 * 8;
+                                int l2 = l1 + k1 * 4;
+                                int i3 = j2 + j1 * 8;
+                                primer.setBlockState(k2, l2, i3, iblockstate);
+                                d15 += d16;
+                            }
+
+                            d10 += d12;
+                            d11 += d13;
+                        }
+
+                        d1 += d5;
+                        d2 += d6;
+                        d3 += d7;
+                        d4 += d8;
+                    }
+                }
+            }
+        }
+    }
+
+    public void buildSurfaces(ChunkPrimer primer, int chunkX, int chunkZ, Biome[] biomeMap)
     {
         if (!ForgeEventFactory.onReplaceBiomeBlocks(this, chunkX, chunkZ, primer, this.world)) return;
         for (int i = 0; i < 16; ++i)
@@ -60,6 +166,7 @@ public class ChunkGeneratorEndBiomes extends ChunkGeneratorEnd
                 Biome current = biomeMap[i + j * 16]; //some backwards ass shit
                 IBlockState surface = END_STONE;
                 IBlockState filler = END_STONE;
+                //if(current == BiomeRegistrar.END_UNDER)  underfill = BlocksRegister.BASOR.getDefaultState();
                 if(current != Biomes.SKY)
                 {
                 	surface = current.topBlock;
@@ -149,7 +256,7 @@ public class ChunkGeneratorEndBiomes extends ChunkGeneratorEnd
 
 
 
-    public float getIslandHeightValue(int p_185960_1_, int p_185960_2_, int p_185960_3_, int p_185960_4_)//делаем публичным, потмо заменю АТ
+    public float getIslandHeightValue(int p_185960_1_, int p_185960_2_, int p_185960_3_, int p_185960_4_)
     {
         float f = (float)(p_185960_1_ * 2 + p_185960_3_);
         float f1 = (float)(p_185960_2_ * 2 + p_185960_4_);
@@ -198,5 +305,80 @@ public class ChunkGeneratorEndBiomes extends ChunkGeneratorEnd
         }
 
         return f2;
+    }
+
+    public double[] getHeights(double[] p_185963_1_, int p_185963_2_, int p_185963_3_, int p_185963_4_, int p_185963_5_, int p_185963_6_, int p_185963_7_)
+    {
+        ChunkGeneratorEvent.InitNoiseField event = new ChunkGeneratorEvent.InitNoiseField(this, p_185963_1_, p_185963_2_, p_185963_3_, p_185963_4_, p_185963_5_, p_185963_6_, p_185963_7_);
+        net.minecraftforge.common.MinecraftForge.EVENT_BUS.post(event);
+        if (event.getResult() == net.minecraftforge.fml.common.eventhandler.Event.Result.DENY) return event.getNoisefield();
+
+        if (p_185963_1_ == null)
+        {
+            p_185963_1_ = new double[p_185963_5_ * p_185963_6_ * p_185963_7_];
+        }
+
+        double d0 = 684.412D;
+        double d1 = 684.412D;
+        d0 *= 2.0D;
+        this.pnr = this.perlinNoise1.generateNoiseOctaves(this.pnr, p_185963_2_, p_185963_3_, p_185963_4_, p_185963_5_, p_185963_6_, p_185963_7_, d0 / 80.0D, 4.277575000000001D, d0 / 80.0D);
+        this.ar = this.lperlinNoise1.generateNoiseOctaves(this.ar, p_185963_2_, p_185963_3_, p_185963_4_, p_185963_5_, p_185963_6_, p_185963_7_, d0, 684.412D, d0);
+        this.br = this.lperlinNoise2.generateNoiseOctaves(this.br, p_185963_2_, p_185963_3_, p_185963_4_, p_185963_5_, p_185963_6_, p_185963_7_, d0, 684.412D, d0);
+        int i = p_185963_2_ / 2;
+        int j = p_185963_4_ / 2;
+        int k = 0;
+
+        for (int l = 0; l < p_185963_5_; ++l)
+        {
+            for (int i1 = 0; i1 < p_185963_7_; ++i1)
+            {
+                float f = this.getIslandHeightValue(i, j, l, i1);
+
+                for (int j1 = 0; j1 < p_185963_6_; ++j1)
+                {
+                    double d2 = this.ar[k] / 512.0D;
+                    double d3 = this.br[k] / 512.0D;
+                    double d5 = (this.pnr[k] / 10.0D + 1.0D) / 2.0D;
+                    double d4;
+
+                    if (d5 < 0.0D)
+                    {
+                        d4 = d2;
+                    }
+                    else if (d5 > 1.0D)
+                    {
+                        d4 = d3;
+                    }
+                    else
+                    {
+                        d4 = d2 + (d3 - d2) * d5;
+                    }
+
+                    d4 = d4 - 8.0D;
+                    d4 = d4 + (double)f;
+                    int k1 = 2;
+
+                    if (j1 > p_185963_6_ / 2 - k1)
+                    {
+                        double d6 = (double)((float)(j1 - (p_185963_6_ / 2 - k1)) / 64.0F);
+                        d6 = MathHelper.clamp(d6, 0.0D, 1.0D);
+                        d4 = d4 * (1.0D - d6) + -3000.0D * d6;
+                    }
+
+                    k1 = 8;
+
+                    if (j1 < k1)
+                    {
+                        double d7 = (double)((float)(k1 - j1) / ((float)k1 - 1.0F));
+                        d4 = d4 * (1.0D - d7) + -30.0D * d7;
+                    }
+
+                    p_185963_1_[k] = d4;
+                    ++k;
+                }
+            }
+        }
+
+        return p_185963_1_;
     }
 }
