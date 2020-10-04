d31099ab87d0ff0e323f3850f7447274e3e063a8
GoogeTan <61940455+GoogeTan@users.noreply.github.com>
2020-10-04 08:35:15 +0300

    2

--- src/main/scala/ru/mousecray/endmagic/world/BiomeDecoratorEndBiomes.java
+++ src/main/scala/ru/mousecray/endmagic/world/BiomeDecoratorEndBiomes.java
@@ -1,10 +1,6 @@
 package ru.mousecray.endmagic.world;
 
-import net.minecraft.world.World;
-import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.BiomeEndDecorator;
-
-import java.util.Random;
 
 public class BiomeDecoratorEndBiomes extends BiomeEndDecorator
 {
