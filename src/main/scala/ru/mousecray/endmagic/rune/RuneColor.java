4c258209935bdf3bf0a6c026c7050441c53b4a56
hohserg <hohserg1@yandex.ru>
2020-11-10 23:57:12 +0300

    Revised elements

--- src/main/scala/ru/mousecray/endmagic/rune/RuneColor.java
+++ src/main/scala/ru/mousecray/endmagic/rune/RuneColor.java
@@ -8,13 +8,15 @@ import ru.mousecray.endmagic.EM;
 import java.awt.*;
 
 public enum RuneColor {
-    light(new Color(255, 255, 200)),
-    fire(new Color(255, 79, 0)),
-    water(new Color(50, 100, 255)),
-    earth(new Color(200, 170, 0)),
-    ender(new Color(27, 123, 107)),
-    air(new Color(200, 255, 200), new ResourceLocation(EM.ID, "runes/test")),
-    darkness(new Color(10, 10, 10), new ResourceLocation(EM.ID, "runes/test"));
+    Light(new Color(255, 255, 200)),
+    Fire(new Color(255, 79, 0)),
+    Water(new Color(50, 100, 255)),
+    Earth(new Color(200, 170, 0)),
+    Void(new Color(27, 123, 107)),
+    Time(new Color(220, 198, 255)),
+    Perception(new Color(181, 0, 63)),
+    Air(new Color(200, 255, 200), new ResourceLocation(EM.ID, "runes/test")),
+    Darkness(new Color(10, 10, 10), new ResourceLocation(EM.ID, "runes/test"));
 
 
     public final int r;
