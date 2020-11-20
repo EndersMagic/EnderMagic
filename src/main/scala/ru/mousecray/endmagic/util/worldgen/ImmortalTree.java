df1d3ec685ddc18bd3c83da83e9ab7d9dbaed236
Wild Tan <61940455+GoogeTan@users.noreply.github.com>
2020-11-20 19:41:16 +0300

    Create ImmortalTree.java

---
+++ src/main/scala/ru/mousecray/endmagic/util/worldgen/ImmortalTree.java
@@ -0,0 +1,17 @@
+package ru.mousecray.endmagic.util.worldgen;
+
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public class ImmortalTree
+{
+    public static void generate(World world, BlockPos pos)
+    {
+
+    }
+
+    private static void generateBranchRecursive(World world, BlockPos pos, int length, int x, int y, int z, boolean isObtuse)
+    {
+
+    }
+}
