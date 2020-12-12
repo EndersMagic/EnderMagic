22a28408f5dab057733aaaf216da3aedb0d4a611
hohserg <hohserg1@yandex.ru>
2020-12-12 11:38:42 +0300

    Added more versions of indicator

--- src/main/scala/ru/mousecray/endmagic/client/render/overlay/EmOverlayIndicator.java
+++ src/main/scala/ru/mousecray/endmagic/client/render/overlay/EmOverlayIndicator.java
@@ -4,12 +4,17 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.ScaledResolution;
+import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
+import net.minecraft.item.Item;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 import net.minecraftforge.fml.relauncher.Side;
+import org.lwjgl.opengl.GL11;
 import ru.mousecray.endmagic.EM;
 import ru.mousecray.endmagic.capability.player.EmCapability;
 import ru.mousecray.endmagic.capability.player.EmCapabilityProvider;
@@ -19,6 +24,8 @@ import ru.mousecray.endmagic.util.render.elix_x.ecomms.color.RGBA;
 
 import java.util.Arrays;
 
+import static java.lang.Math.cos;
+import static java.lang.Math.sin;
 import static net.minecraftforge.client.event.RenderGameOverlayEvent.ElementType.HOTBAR;
 import static org.lwjgl.opengl.GL11.*;
 
@@ -28,12 +35,21 @@ public class EmOverlayIndicator extends Gui {
     private static int visible = 0;
     private static int prevVisible = 0;
     private static final int maxVisible = 20;//ticks
+    private static RuneColor activeColor = RuneColor.Air;
 
     @SubscribeEvent
     public static void onTick(TickEvent.ClientTickEvent event) {
         if (mc().player != null) {
-            //System.out.println("visible " + visible);
-            if ((mc().player.getHeldItemOffhand().getItem() instanceof BaseInscriber || mc().player.getHeldItemMainhand().getItem() instanceof BaseInscriber)) {
+
+            Item itemOffhand = mc().player.getHeldItemOffhand().getItem();
+            Item itemMainhand = mc().player.getHeldItemMainhand().getItem();
+
+            if (itemOffhand instanceof BaseInscriber || itemMainhand instanceof BaseInscriber) {
+                if (itemMainhand instanceof BaseInscriber)
+                    activeColor = ((BaseInscriber) itemMainhand).runeColor();
+                else
+                    activeColor = ((BaseInscriber) itemOffhand).runeColor();
+
                 if (visible < maxVisible)
                     visible++;
             } else if (visible > 0)
@@ -49,18 +65,130 @@ public class EmOverlayIndicator extends Gui {
     public static void renderGameOverlay(RenderGameOverlayEvent.Post event) {
         if (event.getType() == HOTBAR) {
 
-            float colorAlpha = (prevVisible + (visible - prevVisible) * mc().getRenderPartialTicks()) / maxVisible;
-            renderIndicatorLines(colorAlpha);
-            //renderIndicatorCircleOne();
-            //renderIndicatorCircleTwo();
-            GlStateManager.color(1, 1, 1, 1);
+            GlStateManager.pushMatrix();
+            {
+                float colorAlpha = (prevVisible + (visible - prevVisible) * mc().getRenderPartialTicks()) / maxVisible;
+                renderIndicatorLines(colorAlpha);
+                //renderIndicatorCircleOne();
+                //renderIndicatorCircleTwo();
+                GlStateManager.color(1, 1, 1, 1);
+            }
+            GlStateManager.popMatrix();
         }
     }
 
-    private static void renderIndicatorCircleTwo() {
+    private static void renderIndicatorCircleOne() {
+        GlStateManager.depthMask(false);
+        GlStateManager.disableTexture2D();
+
+        GlStateManager.enableBlend();
+        GlStateManager.blendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+        GlStateManager.enableAlpha();
+        GlStateManager.alphaFunc(GL_GREATER, 0);
+
+        EmCapability capa = EmCapabilityProvider.getCapa(mc().player);
+
+        //renderProgressPie(0,0,1);
+        double segmentAngle = Math.PI * 2 / RuneColor.values().length;
+        double angleOffset = (Math.PI / 2 - segmentAngle) / 2;
+
+        GlStateManager.translate(20, 20, 0);
+
+        for (int i = 0; i < RuneColor.values().length; i++) {
+            RuneColor runeColor = RuneColor.values()[i];
+            int em = capa.getEm(runeColor);
+            int maxEm = capa.getMaxEm(runeColor);
+            renderEmSegmentOne(runeColor, em, maxEm, segmentAngle * i - segmentAngle * Arrays.asList(RuneColor.values()).indexOf(activeColor) + angleOffset);
+        }
+
+
+        GlStateManager.enableTexture2D();
+        GlStateManager.depthMask(true);
+
     }
 
-    private static void renderIndicatorCircleOne() {
+    private static void renderEmSegmentOne(RuneColor runeColor, int em, int maxEm, double additionRotation) {
+        double segmentAngle = Math.PI * 2 / RuneColor.values().length;
+
+        double radius = 0.02 * maxEm;
+
+        int segmentDetalization = 100;
+
+        BufferBuilder buf = Tessellator.getInstance().getBuffer();
+        buf.begin(GL11.GL_TRIANGLE_FAN, DefaultVertexFormats.POSITION_COLOR);
+        buf.pos(0, 0, 0).color(runeColor.r, runeColor.g, runeColor.b, 128).endVertex();
+        for (int i = segmentDetalization; i > 0; i--) {
+            double angle = (double) i / segmentDetalization * segmentAngle + additionRotation;
+            buf.pos(radius * cos(angle), radius * sin(angle), 0).color(runeColor.r, runeColor.g, runeColor.b, 128).endVertex();
+        }
+        buf.pos(0, 0, 0).color(runeColor.r, runeColor.g, runeColor.b, 128).endVertex();
+        Tessellator.getInstance().draw();
+
+        buf.begin(GL11.GL_TRIANGLE_FAN, DefaultVertexFormats.POSITION_COLOR);
+        buf.pos(0, 0, 0).color(runeColor.r, runeColor.g, runeColor.b, 128).endVertex();
+        for (int i = (int) (segmentDetalization * ((double) em / maxEm)); i > 0; i--) {
+            double angle = (double) i / segmentDetalization * segmentAngle + additionRotation;
+            buf.pos(radius * cos(angle), radius * sin(angle), 0).color(runeColor.r, runeColor.g, runeColor.b, 128).endVertex();
+        }
+        buf.pos(0, 0, 0).color(runeColor.r, runeColor.g, runeColor.b, 128).endVertex();
+        Tessellator.getInstance().draw();
+
+    }
+
+    private static void renderIndicatorCircleTwo() {
+        GlStateManager.depthMask(false);
+        GlStateManager.disableTexture2D();
+
+        GlStateManager.enableBlend();
+        GlStateManager.blendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+        GlStateManager.enableAlpha();
+        GlStateManager.alphaFunc(GL_GREATER, 0);
+
+        EmCapability capa = EmCapabilityProvider.getCapa(mc().player);
+
+        //renderProgressPie(0,0,1);
+        double segmentAngle = Math.PI * 2 / RuneColor.values().length;
+        double angleOffset = (Math.PI / 2 - segmentAngle) / 2;
+
+        GlStateManager.translate(20, 20, 0);
+
+        for (int i = 0; i < RuneColor.values().length; i++) {
+            RuneColor runeColor = RuneColor.values()[i];
+            int em = capa.getEm(runeColor);
+            int maxEm = capa.getMaxEm(runeColor);
+            renderEmSegmentTwo(runeColor, em, maxEm, segmentAngle * i - segmentAngle * Arrays.asList(RuneColor.values()).indexOf(activeColor) + angleOffset);
+        }
+
+
+        GlStateManager.enableTexture2D();
+        GlStateManager.depthMask(true);
+    }
+
+    private static void renderEmSegmentTwo(RuneColor runeColor, int em, int maxEm, double additionRotation) {
+        double segmentAngle = Math.PI * 2 / RuneColor.values().length;
+
+        double radius = 0.02 * maxEm;
+
+        int segmentDetalization = 100;
+
+        BufferBuilder buf = Tessellator.getInstance().getBuffer();
+        buf.begin(GL11.GL_TRIANGLE_FAN, DefaultVertexFormats.POSITION_COLOR);
+        buf.pos(0, 0, 0).color(runeColor.r, runeColor.g, runeColor.b, 128).endVertex();
+        for (int i = segmentDetalization; i > 0; i--) {
+            double angle = (double) i / segmentDetalization * segmentAngle + additionRotation;
+            buf.pos(radius * cos(angle), radius * sin(angle), 0).color(runeColor.r, runeColor.g, runeColor.b, 128).endVertex();
+        }
+        buf.pos(0, 0, 0).color(runeColor.r, runeColor.g, runeColor.b, 128).endVertex();
+        Tessellator.getInstance().draw();
+
+        buf.begin(GL11.GL_TRIANGLE_FAN, DefaultVertexFormats.POSITION_COLOR);
+        buf.pos(0, 0, 0).color(runeColor.r, runeColor.g, runeColor.b, 128).endVertex();
+        for (int i = segmentDetalization; i > 0; i--) {
+            double angle = (double) i / segmentDetalization * segmentAngle + additionRotation;
+            buf.pos(radius * ((double) em / maxEm) * cos(angle), radius * ((double) em / maxEm) * sin(angle), 0).color(runeColor.r, runeColor.g, runeColor.b, 128).endVertex();
+        }
+        buf.pos(0, 0, 0).color(runeColor.r, runeColor.g, runeColor.b, 128).endVertex();
+        Tessellator.getInstance().draw();
 
     }
 
