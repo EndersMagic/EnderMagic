452323979d96639cbb069de0cc6709b12a9ea875
hohserg <hohserg1@yandex.ru>
2020-12-14 17:43:50 +0300

    Fix hashCode of AttributeId

--- src/main/scala/ru/mousecray/endmagic/util/render/endothermic/format/AttributeId.scala
+++ src/main/scala/ru/mousecray/endmagic/util/render/endothermic/format/AttributeId.scala
@@ -2,6 +2,14 @@ package ru.mousecray.endmagic.util.render.endothermic.format
 
 import net.minecraft.client.renderer.vertex.VertexFormatElement
 
+import scala.runtime.Statics
+
 case class AttributeId(_1: VertexFormatElement, _2: Int, _3: VertexRepr) {
-  override lazy val hashCode: Int = super.hashCode()
+  override lazy val hashCode: Int =  {
+    var i = -889275714
+    i = Statics.mix(i, Statics.anyHash(_1))
+    i = Statics.mix(i, _2)
+    i = Statics.mix(i, Statics.anyHash(_3))
+    Statics.finalizeHash(i, 3)
+  }
 }
