f419a748f6cc068edd02c79edb3b299593b8102b
hohserg <hohserg1@yandex.ru>
2021-01-09 14:40:06 +0300

    Cleanup

--- src/main/scala/ru/mousecray/endmagic/tileentity/ByBlockNotifiable.java
+++ src/main/scala/ru/mousecray/endmagic/tileentity/ByBlockNotifiable.java
@@ -1,6 +1,6 @@
 package ru.mousecray.endmagic.tileentity;
 
 public interface ByBlockNotifiable {
-    public void neighborChanged();
-    public void breakBlock();
+    void neighborChanged();
+    void breakBlock();
 }
