0b37cd9c5536df9dfbb9469b029542ecf4ef8072
hohserg <hohserg1@yandex.ru>
2021-01-09 04:33:49 +0300

    F ix serialization of portal capability

--- src/main/scala/ru/mousecray/endmagic/capability/chunk/portal/PortalCapabilityStorage.java
+++ src/main/scala/ru/mousecray/endmagic/capability/chunk/portal/PortalCapabilityStorage.java
@@ -1,7 +1,7 @@
 package ru.mousecray.endmagic.capability.chunk.portal;
 
 import net.minecraft.nbt.NBTBase;
-import net.minecraft.nbt.NBTTagInt;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
@@ -15,10 +15,12 @@ public class PortalCapabilityStorage implements Capability.IStorage<PortalCapabi
     public NBTBase writeNBT(Capability<PortalCapability> capability, PortalCapability instance, EnumFacing side) {
         NBTTagList r = new NBTTagList();
         instance.masterPosToHeight.forEach((pos, height) -> {
-            int encodedPos = (pos.getY() << 8) & ((pos.getX() % 16) << 4) & (pos.getZ() % 16);
-            System.out.println("writeNBT " + encodedPos + " " + pos);
-            r.appendTag(new NBTTagInt(encodedPos));
-            r.appendTag(new NBTTagInt(height));
+            NBTTagCompound entry = new NBTTagCompound();
+            entry.setInteger("x", pos.getX());
+            entry.setInteger("y", pos.getY());
+            entry.setInteger("z", pos.getZ());
+            entry.setInteger("height", height);
+            r.appendTag(entry);
         });
         return r;
     }
@@ -26,16 +28,12 @@ public class PortalCapabilityStorage implements Capability.IStorage<PortalCapabi
     @Override
     public void readNBT(Capability<PortalCapability> capability, PortalCapability instance, EnumFacing side, NBTBase nbt) {
         instance.masterPosToHeight.clear();
-        int startX = instance.chunk.x;
-        int startZ = instance.chunk.z;
 
         NBTTagList nbtList = (NBTTagList) nbt;
+
         for (int i = 0; i < nbtList.tagCount(); i += 2) {
-            int encodedPos = nbtList.getIntAt(i);
-            int height = nbtList.getIntAt(i + 1);
-            BlockPos pos = new BlockPos(startX + ((encodedPos >> 4) & 0xf), encodedPos >> 8, startZ + (encodedPos & 0xf));
-            System.out.println("readNBT " + encodedPos + " " + pos);
-            instance.masterPosToHeight.put(pos, height);
+            NBTTagCompound entry = nbtList.getCompoundTagAt(i);
+            instance.masterPosToHeight.put(new BlockPos(entry.getInteger("x"), entry.getInteger("y"), entry.getInteger("z")), entry.getInteger("height"));
         }
     }
 }
