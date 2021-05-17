4f8773727be25fee26431087ed24421322edb050
hohserg <hohserg1@yandex.ru>
2021-05-17 14:20:30 +0300

    Update RuneShaper.java

--- src/main/scala/ru/mousecray/endmagic/items/RuneShaper.java
+++ src/main/scala/ru/mousecray/endmagic/items/RuneShaper.java
@@ -18,14 +18,15 @@ import java.util.Map;
 public class RuneShaper extends Item {
     @Override
     public EnumActionResult onItemUse(EntityPlayer player, World world, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
-        RuneIndex.getRune(world, pos, facing).ifPresent(this::println);
+        if (!world.isRemote)
+            RuneIndex.getRune(world, pos, facing).ifPresent(this::println);
         return EnumActionResult.SUCCESS;
     }
 
     private void println(Rune rune) {
         Map<Vec2i, RunePart> parts = JavaConversions.mapAsJavaMap(rune.parts());
-        StringBuilder useful = new StringBuilder("ImmutableMap.build()\n");
-        parts.forEach((coord, part) -> useful.append(".add(new Vec2i(" + coord.x + ", " + coord.y + "), new RunePart(" + part.color() + "))\n"));
+        StringBuilder useful = new StringBuilder("ImmutableMap.<Vec2i, RunePart>builder()\n");
+        parts.forEach((coord, part) -> useful.append(".put(new Vec2i(" + coord.x + ", " + coord.y + "), new RunePart(" + part.color().name() + "))\n"));
         useful.append(".build()\n");
         System.out.println(useful.toString());
     }
