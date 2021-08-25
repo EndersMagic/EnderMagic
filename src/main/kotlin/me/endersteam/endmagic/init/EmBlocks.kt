6f2b472f1712df89e54b6b397fb7bdf194c34f3b
Wild Tan <61940455+GoogeTan@users.noreply.github.com>
2021-08-25 20:01:52 +0300

    a

--- src/main/kotlin/me/endersteam/endmagic/init/EmBlocks.kt
+++ src/main/kotlin/me/endersteam/endmagic/init/EmBlocks.kt
@@ -8,17 +8,18 @@ import net.minecraftforge.registries.ForgeRegistries
 
 object EmBlocks : AutoRegisterer<Block>(ForgeRegistries.BLOCKS)
 {
-    val dragonCoal: Block by register("dragon_coal_block", defaultBlock())
-    val naturalCoal: Block by register("natural_coal_block", defaultBlock())
-    val phantomCoal: Block by register("phantom_coal_block", defaultBlock())
-    val immortalCoal: Block by register("immortal_coal_block", defaultBlock())
+    val dragonCoal: Block by register("dragon_coal_block", defaultCoalBlock())
+    val naturalCoal: Block by register("natural_coal_block", defaultCoalBlock())
+    val phantomCoal: Block by register("phantom_coal_block", defaultCoalBlock())
+    val immortalCoal: Block by register("immortal_coal_block", defaultCoalBlock())
 
-    val dragonSteel: Block by register("dragon_steel_block", defaultBlock())
-    val naturalSteel: Block by register("natural_steel_block", defaultBlock())
-    val phantomSteel: Block by register("phantom_steel_block", defaultBlock())
-    val immortalSteel: Block by register("immortal_steel_block", defaultBlock())
+    val dragonSteel: Block by register("dragon_steel_block", defaultSteelBlock())
+    val naturalSteel: Block by register("natural_steel_block", defaultSteelBlock())
+    val phantomSteel: Block by register("phantom_steel_block", defaultSteelBlock())
+    val immortalSteel: Block by register("immortal_steel_block", defaultSteelBlock())
 
     val blastFurnace : Block by register("blast_furnace", BlastFurnaceBlock)
 
-    fun defaultBlock() = Block(AbstractBlock.Properties.create(Material.IRON).hardnessAndResistance(9f))
+    fun defaultCoalBlock() = Block(AbstractBlock.Properties.create(Material.ROCK).hardnessAndResistance(6f))
+    fun defaultSteelBlock() = Block(AbstractBlock.Properties.create(Material.IRON).hardnessAndResistance(9f))
 }
